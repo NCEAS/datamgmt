5b9b7ba471a89ee7af920b78ef471ec9d679ff6e
maier-m <mmaier@ucsb.edu>
2018-01-24 13:14:38 -0800

    Allow user inputs

--- R/create_attributes_table.R
+++ R/create_attributes_table.R
@@ -1,7 +1,7 @@
-#' Returns the numberType (either 'real', 'integer', 'whole', or 'natural') of input values
+#' Returns the numberType (either "real", "integer", "whole", or "natural") of input values
 #'
 #' @param values A vector of values. If vector is non-numeric will return NA
-#' @return The numberType of \code{values} (either 'real', 'integer', 'whole', or 'natural').
+#' @return The numberType of \code{values} (either "real", "integer", "whole", or "natural").
 #' @examples
 #' # To get numberType for each column in a data.frame \code{df}:
 #' apply(df, 2, function(x) get_numberType(x))
@@ -9,15 +9,15 @@ get_numberType <- function(values){
     values <- unlist(values)
     numberType=NA
     if (is.numeric(values)){
-        if (all(is.nan(values))){numberType='real'}
+        if (all(is.nan(values))){numberType="real"}
         else{
-            if (any(round(values)!=values)){numberType='real'}
+            if (any(round(values)!=values)){numberType="real"}
             else{
-                if (any(values<0,na.rm = T)){numberType='integer'}
+                if (any(values<0,na.rm = T)){numberType="integer"}
                 else{
-                    if (any(values==0,na.rm = T)){numberType='whole'}
+                    if (any(values==0,na.rm = T)){numberType="whole"}
                     else{
-                        numberType='natural'
+                        numberType="natural"
                     }
                 }
             }
@@ -27,207 +27,143 @@ get_numberType <- function(values){
 }
 
 shiny_AttributeTable <- function(att_table){
-  require(shiny)
-  require(rhandsontable)
-  # ----------
-  #UI
-  ui <- fluidPage(
-    rHandsontableOutput("table"),
-    actionButton("print", "Print Code")
-  )
-
-  server <- function(input, output) {
-
-    DF_R = reactive({
-      if (is.null(input$table)) {
-        DF = att_table}else{
-          DF = hot_to_r(input$table)
-        }
-    })
-
-    output$table=renderRHandsontable({
-      rhandsontable(DF_R())%>%
-        hot_cols(renderer = "
-           function (instance, td, row, col, prop, value, cellProperties) {
-                                    Handsontable.renderers.NumericRenderer.apply(this, arguments);
-                                    if (row%2 ==0) {
-                                    td.style.background = '#e8e8e8';
-                                    }
-    }")
-    })
-
-    observeEvent(input$print, {
-      DF_out <- DF_R()
-      DF_out$domain <- as.character(DF_out$domain)
-      DF_out[DF_out == ''] = NA
-      DF_out$definition = ifelse(DF_out$definition =='',DF_out$attributeDefinition,DF_out$definition)
-      output_text <- c()
-      for(c in colnames(DF_out)){
-        output_text[c] <- paste0(c,' = ',DF_out[c],',\n')}
-      output_text[length(output_text)] <- sub('[,]\\n$','',output_text[length(output_text)])
-      # ---
-      cat('\n\ndata.frame(\n',
-          output_text,
-          ')\n\n')
-    })
-
-  }
-
-  shinyApp(ui, server)
-}
-
-create_attributes_table <- function(df, is.attribute.table = F) {
-  require(dplyr)
-
-  if (is.attribute.table ==F){
-    if(!length((colnames(df)))) {
-      stop("column names be populated")
-    }
-
-    col_names <- colnames(df)
-    n <- length(col_names)
-
-    if (any(grepl(" ", col_names))) {
-      stop(paste0("column names cannot contain whitespace"))
-    }
-    # ----------
-    # Get numberType of dataframe
-    numberType <- c()
-    for (c in 1:n){
-      numberType[c] <- get_numberType(df[,c])
-    }
-    # ----------
-
-    att_table <- data.frame(attributeName = col_names,
-                            attributeDefinition = rep('', n),
-                            unit = rep('', n),
-                            measurementScale = rep('', n),
-                            domain = factor(ifelse(is.na(numberType),NA,'numericDomain'),
-                                                         levels = c('numericDomain',
-                                                                    'textDomain',
-                                                                    'enumeratedDomain',
-                                                                    'dateTimeDomain')),
-                            formatString = rep('', n),
-                            definition = factor(rep('A', n),levels = letters[10:1]),
-                            numberType = numberType,
-                            missingValueCode = rep('', n),
-                            missingValueCodeExplanation = rep('', n),
-                            stringsAsFactors = F)
-  }else
-  {att_table=df}
-
-  shiny_AttributeTable(att_table)
-}
-
-
-
-=======
-    numberType=NA
-    if (is.numeric(values)){
-        if (any(values%%1!=0,na.rm = T)){numberType='real'}
-        else{
-            if (any(values<0,na.rm = T)){numberType='integer'}
-            else{
-                if (any(values==0,na.rm = T)){numberType='whole'}
-                else{
-                    numberType='natural'
+    ### Libraries
+    require(shiny)
+    require(rhandsontable)
+    ### UI
+    ui <- fluidPage(
+        h3("Edit attribute table as needed, then print attribute table code to console."),
+        rHandsontableOutput("table"),
+        actionButton("print", "Print Code")
+    )
+    ### Server
+    server <- function(input, output) {
+        # Attribute Table Reactive
+        DF_R = reactive({
+            if (is.null(input$table)) {
+                att_table}else{
+                    hot_to_r(input$table)
                 }
-            }
-        }
+        })
+        # Attribute Table Interface
+        output$table=renderRHandsontable({
+            rhandsontable(DF_R())%>%
+                hot_cols(renderer = "
+                         function (instance, td, row, col, prop, value, cellProperties) {
+                         Handsontable.renderers.NumericRenderer.apply(this, arguments);
+                         if (row%2 ==0) {
+                         td.style.background = '#e8e8e8';
+                         }
+                         }")
+        })
+        # Output from print command
+        observeEvent(input$print, {
+            DF_out <- DF_R()
+            DF_out$domain <- as.character(DF_out$domain)
+            DF_out[DF_out == ""] = NA
+            DF_out$definition = ifelse(DF_out$definition =="",DF_out$attributeDefinition,DF_out$definition)
+            output_text <- c()
+            for(c in colnames(DF_out)){
+                output_text[c] <- paste0(c," = ",DF_out[c],",\n")}
+            output_text[length(output_text)] <- sub("[,]\\n$","",output_text[length(output_text)])
+            # ---
+            cat("\n\ndata.frame(\n",
+                output_text,
+                ")\n\n")
+        })
     }
-    return(numberType)
+    shinyApp(ui, server)
 }
 
-create_attributes_table <- function(data, definitions = NULL, missingValueCode = NULL,
-                                    missingValueCodeExplanation = NULL) {
-    if(!length((colnames(data)))) {
+create_attributes_table <- function(df,
+                                    attributeDefinition = NULL,
+                                    unit = NULL,
+                                    measurementScale = NULL,
+                                    domain = NULL,
+                                    formatString = NULL,
+                                    definition = NULL,
+                                    numberType = NULL,
+                                    missingValueCode = NULL,
+                                    missingValueCodeExplanation = NULL){
+    ### Libraries
+    require(dplyr)
+    # Get column names
+    if(!length((colnames(df)))){
         stop("column names be populated")
     }
-
-    col_names <- colnames(data)
+    col_names <- colnames(df)
     n <- length(col_names)
-
-    if (any(grepl(" ", col_names))) {
-        stop(paste0("column names cannot contain whitespace"))
+    # Get numberType
+    numberType_levels <- c("real","natural","whole","integer")
+    if (is.null(numberType)){
+        numberType <- paste0(apply(df, 2, function(x) get_numberType(x)))}
+    if (length(numberType)!=n){
+        stop("numberType is not the same length as input data frame")
     }
-    if (!(is.null(definitions))) {
-        if (length(definitions) != n) {
-            stop(paste0("definitions has length = ", length(definitions), ", rather than ", n))
-        }
-    } else {
-        definitions = rep(NA, n)
+    numberType <- factor(numberType,levels=numberType_levels)
+    # Get attributeDefinition
+    if (is.null(attributeDefinition)){
+        attributeDefinition <- rep("", n)}
+    if (length(attributeDefinition)!=n){
+        stop("attributeDefinition is not the same length as input data frame")
     }
-    if (!(is.null(missingValueCode))) {
-        stopifnot(is.character(missingValueCode))
+    # Get unit
+    if (is.null(unit)){
+        unit <- rep("", n)}
+    if (length(unit)!=n){
+        stop("unit is not the same length as input data frame")
     }
-    if (!(is.null(missingValueCodeExplanation))) {
-        stopifnot(is.character(missingValueCodeExplanation))
+    # Get measurementScale
+    measurementScale_levels <- c("nominal","ordinal","dateTime", "ratio","interval")
+    if (is.null(measurementScale)){
+        measurementScale <- rep("", n)}
+    if (length(measurementScale)!=n){
+        stop("measurementScale is not the same length as input data frame")
     }
-
-    table <- data.frame(attributeName = col_names,
-                        attributeDefinition = definitions,
-                        measurementScale = rep("NA", n),
-                        domain = rep("NA", n),
-                        formatString = rep("NA", n),
-                        definition = definitions,
-                        unit = rep("NA", n),
-                        numberType = rep("NA", n),
-                        missingValueCode = rep("NA", n),
-                        missingValueCodeExplanation = rep("NA", n),
-                        stringsAsFactors = F)
-
-    for (i in seq_len(n)) {
-
-        if (is.na(table$attributeDefinition[i])) {
-            table$attributeDefinition[i] <- readline(prompt = paste0("Enter attribute definition for ",
-                                                                     table$attributeName[i],": \n"))
-            table$definition[i] <- table$attributeDefinition[i]
-        }
-
-        measurementScale1 <- readline(prompt = paste0("Enter measurement scale for ", table$attributeName[i],
-                                                      ". Choose one of: \n dateTime: 'd' \n interval: 'i' \n nominal: 'n' \n ordinal: 'o' \n ratio: 'r' \n"))
-
-        switch(measurementScale1,
-               "d" = {
-                   table$measurementScale[i] = "dateTimeDomain"
-               },
-               "i" = {
-                   table$measurementScale[i] = "interval"
-               },
-               "n" = {
-                   table$measurementScale[i] = "nominal"
-               },
-               "o" = {
-                   table$measurementScale[i] = "ordinal"
-               },
-               "r" = {
-                   table$measurementScale[i] = "ratio"
-               })
-
-        if (table$measurementScale[i] %in% c("nominal", "ordinal")) {
-            table$domain[i] = "textDomain"
-        } else if (table$measurementScale[i] %in% c("ratio", "interval")) {
-            table$domain[i] = "numericDomain"
-        } else {
-            table$domain[i] = "dateTimeDomain"
-        }
-
-        if (table$domain[i] == "dateTimeDomain") {
-            table$formatString[i] <- readline(prompt = paste0("Enter string format for ", table$attributeName[i],
-                                                              ": \nExample: 'MM/DD/YYYY'"))
-        } else if (table$domain[i] == "numericDomain") {
-            table$unit[i] <- readline(prompt = paste0("Enter units of measurement for ", table$attributeName[i],
-                                                      ": \n"))
-            table$numberType[i] <- get_numberType(data[,i])
-        } else {
-            table$unit[i] = NA
-            table$numberType[i] = NA
-        }
-
+    measurementScale <- factor(measurementScale,levels=measurementScale_levels)
+    # Get domain
+    domain_levels <- c("numericDomain","textDomain","enumeratedDomain","dateTimeDomain")
+    if (is.null(domain)){
+        domain <- ifelse(is.na(numberType),"","numericDomain")}
+    if (length(domain)!=n){
+        stop("domain is not the same length as input data frame")
+    }
+    domain <- factor(domain,levels=domain_levels)
+    # Get formatString
+    if (is.null(formatString)){
+        formatString <- rep("", n)}
+    if (length(formatString)!=n){
+        stop("formatString is not the same length as input data frame")
+    }
+    # Get definition
+    if (is.null(definition)){
+        definition <- rep("", n)}
+    if (length(definition)!=n){
+        stop("definition is not the same length as input data frame")
+    }
+    # Get missingValueCode
+    if (is.null(missingValueCode)){
+        missingValueCode <- rep("", n)}
+    if (length(missingValueCode)!=n){
+        stop("missingValueCode is not the same length as input data frame")
+    }
+    # Get missingValueCodeExplanation
+    if (is.null(missingValueCodeExplanation)){
+        missingValueCodeExplanation <- rep("", n)}
+    if (length(missingValueCodeExplanation)!=n){
+        stop("missingValueCodeExplanation is not the same length as input data frame")
     }
 
-    message("If any nonimal or ordinal variables are 'enumeratedDomain', these were incorrectly set to 'textDomain'. Please update these fields by hand.")
-
-    return(table)
-
+    att_table <- data.frame(attributeName = col_names,
+                            attributeDefinition = attributeDefinition,
+                            unit = unit,
+                            measurementScale = measurementScale,
+                            domain = domain,
+                            formatString = formatString,
+                            definition = definition,
+                            numberType = numberType,
+                            missingValueCode = rep("", n),
+                            missingValueCodeExplanation = rep("", n),
+                            stringsAsFactors = F)
+    shiny_AttributeTable(att_table)
 }
