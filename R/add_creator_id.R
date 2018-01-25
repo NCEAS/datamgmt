dfcfaee8449a00457df2ca703f83100f10680b5c
Irene <25118334+isteves@users.noreply.github.com>
2018-01-25 12:31:35 -0800

    made changes suggeested by dominic

--- R/add_creator_id.R
+++ R/add_creator_id.R
@@ -5,32 +5,49 @@
 #' @param orcid ORCID in the format 'https://orcid.org/WWWW-XXXX-YYYY-ZZZZ'
 #' @param id reference ID. Character string to set reference ID for creators with additional roles (i.e. metadataProvider, etc.)
 #' @param surname creator surname (last name), defaults to first creator if not specified. Not case-sensitive.
-#' @keywords eml creator orcid id
-#' @details All prameters other than the EML are optional, but since the point of the function is to modify either the orcid, ref id, or both, you better specify at least one. Requires the crayon package.
-#' @export
-#' @examples
-#' add_creator_id(eml2, orcid = "https://orcid.org/WWWW-XXXX-YYYY-ZZZZ")
 #'
+#' @keywords eml creator orcid id
+#'
+#' @details All prameters other than the EML are optional, but since the point of the function is to modify either the orcid, ref id, or both, you better specify at least one. Requires the crayon package.
+#'
+#' @export
+#'
+#' @examples
+#' library(dataone)
+#' library(arcticdatautils)
+#' library(EML)
+#'
+#' cnTest <- dataone::CNode('STAGING')
+#' mnTest <- dataone::getMNode(cnTest,'urn:node:mnTestARCTIC')
+#' eml_pid <- arcticdatautils::create_dummy_metadata(mnTest)
+#' eml1 <- EML::read_eml(rawToChar(getObject(mnTest, eml_pid)))
+#' add_creator_id(eml1, orcid = "https://orcid.org/WWWW-XXXX-YYYY-ZZZZ")
 
-#function for adding orcid or reference id to creator
 add_creator_id <- function(eml,
                            orcid = NULL,
                            id = NULL,
                            surname = NULL) { #not sensitive to capitalization
     library(crayon)
 
+    #variable checks:
+    for (args in c(orcid, id, surname)) {
+        if (!(is.null(args) | is.character(args))) {
+            stop(paste(args, "must be a character string."))
+        }
+    }
+
     #grab creators and save to new variable
     creatorList <- eml@dataset@creator@.Data
 
     #determine surname position to access correct creator
     #if none are specified, the first creator will be modified
-    if(is.null(surname == TRUE)){
+    if (is.null(surname)) {
         cat(green("Since surname was not specified, the first creator entry will be modified. "))
         pos <- 1
     } else {
         #make vector of creator surnames
         surNames <- rep(NA, times = length(creatorList))
-        for(i in 1:length(creatorList)){
+        for (i in 1:length(creatorList)) {
             creator1 <- creatorList[[i]]
             surName1 <- creator1@individualName@.Data[[1]]@surName@.Data
             surNames[i] <- surName1
@@ -39,7 +56,7 @@ add_creator_id <- function(eml,
         #if specified surname exists, then edit entry (caps-proof)
         surname_u <- toupper(surname)
         surNames_u <- toupper(surNames)
-        if(surname_u %in% surNames_u == TRUE){
+        if (surname_u %in% surNames_u) {
             pos <- which(surNames_u == surname_u)
         } else {
             stop(red("Surname not found. Check your spelling."))
@@ -47,14 +64,14 @@ add_creator_id <- function(eml,
     }
 
     #add orcid if specified
-    if(is.null(orcid) == FALSE){
+    if (!is.null(orcid)) {
         creatorList[[pos]]@userId <- c(new('userId',
                                            .Data = orcid,
                                            directory = "https://orcid.org"))
     }
 
     #add reference id if specified
-    if(is.null(id) == FALSE){
+    if (!is.null(id)) {
         creatorList[[pos]]@id[[1]] <- as.character(id)
     }
 
