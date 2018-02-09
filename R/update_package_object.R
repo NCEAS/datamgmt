3b8797a5ea2094af2c9c8709b8099757ce84a51d
Irene <25118334+isteves@users.noreply.github.com>
2018-02-09 08:45:29 -0800

    made changes suggested by dom

--- R/update_package_object.R
+++ R/update_package_object.R
@@ -1,14 +1,21 @@
-#' update_package_object
+#' Update a data object and accompanying package resource map
 #'
 #' This function updates an data object, and then automatically
 #' updates the package resource map with the new data pid. It
-#' is a convenience wrapper around 'arcticdatautils::update_object'
-#' and 'arcticdatautils::update_resource_map'.
+#' is a convenience wrapper around \code{\link{arcticdatautils::update_object}}
+#' and \code{\link{arcticdatautils::update_resource_map}}.
 #'
 #' @param mn (MNode) Member node
 #' @param data_pid (character) PID for data object to update
 #' @param new_data_path (character) Path to new data object
-#' @param rm_pid (character) PID for resource map to update.
+#' @param resource_map_pid (character) PID for resource map to update
+#' @param format_id (character) Optional. The format ID to set for the object.
+#' When not set, \code{\link{arcticdatautils::guess_format_id}} will be used
+#' to guess the format ID. Should be a \href{https://cn.dataone.org/cn/v2/formats}{DataONE format ID}.
+#' @param public (logical) Optional. Make the update public. If FALSE,
+#' will set the metadata and resource map to private (but not the data objects).
+#' This applies to the new metadata PID and its resource map and data object.
+#' Access policies are not affected.
 #'
 #' @keywords update_object update_resource_map
 #'
@@ -17,33 +24,36 @@
 update_package_object <- function(mn,
                            data_pid,
                            new_data_path,
-                           rm_pid,
+                           resource_map_pid,
                            format_id = NULL,
                            public = FALSE) {
 
     #argument checks:
     library(assertthat)
-    assert_that(is(mnTest, "MNode"),
+    library(arcticdatautils)
+    assertthat::assert_that(is(mnTest, "MNode"),
                 is.character(data_pid),
                 is.character(new_data_path),
-                is.character(rm_pid))
+                file.exists(new_data_path),
+                is.character(resource_map_pid),
+                is.logical(public))
 
-    new_data_pid <- update_object(mn,
+    new_data_pid <- arcticdatautils::update_object(mn,
                                   data_pid,
                                   new_data_path,
                                   format_id = format_id)
 
-    pkg <- get_package(mn, rm_pid)
+    pkg <- arcticdatautils::get_package(mn, resource_map_pid)
     other_data_pids <- pkg$data[pkg$data != data_pid]
     new_data_pids <- c(other_data_pids,
                        new_data_pid)
 
-    new_rm <- update_resource_map(mn,
+    new_resource_map_pid <- arcticdatautils::update_resource_map(mn,
                         pkg$resource_map,
                         pkg$metadata,
                         new_data_pids,
                         public = public)
 
     #output package info
-    get_package(mn, new_rm)
+    get_package(mn, new_resource_map_pid)
 }
