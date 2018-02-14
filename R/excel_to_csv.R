de3cae5e64f2d4aa8c10d85513b687003ff06a57
Dominic Mullen <dmullen17@gmail.com>
2018-02-14 15:27:20 -0800

    first version of excel_to_csv

---
+++ R/excel_to_csv.R
@@ -0,0 +1,50 @@
+#' Convert excel workbook to multiple csv files
+#'
+#' Converts an excel workbook into multiple csv files (one per tab).  Names the
+#' files in the following format: sheetName_excelName.csv.
+#'
+#' @param path (character) File location of the excel workbook.
+#' @param directory (character) Optional.  Directory to download csv files to.
+#' Defaults to the base directory that \code{path} is located in.
+#'
+#' @export
+#'
+#' @author Dominic Mullen \email{dmullen17@@gmail.com}
+#'
+#' @return (invisible())
+excel_to_csv <- function(path, directory = NULL, ...) {
+    # Stop if the user doesn't have the readxl package installed
+    if (!requireNamespace("readxl")) {
+        stop(call. = FALSE,
+             "The readxl package is required to convert excel workbooks to csv. Please install it and try again.")
+    }
+
+    stopifnot(file.exists(path))
+    if (!is.null(directory)) {
+        stopifnot(file.exists(directory))
+    } else {
+        directory = dirname(path)
+    }
+
+    excel_name <- basename(path)
+    excel_name <- gsub(".xlsx", "", excel_name, ignore.case = TRUE)
+    excel_name <- gsub(".xls", "", excel_name, ignore.case = TRUE)
+
+    # Try to read excel file and split into csvs
+    tryCatch({
+        sheets <- excel_sheets(path)
+
+        lapply(seq_along(sheets), function(i) {
+            csv = read_excel(path, sheet = sheets[i])
+
+            file_name <- paste0(sheets[i], "_", excel_name, ".csv")
+            file_path <- file.path(directory, file_name)
+
+            write.csv(csv, file_path , row.names = FALSE)})
+
+    },
+    error = function(e) {message("Error converting: ", path, " to csv\n")}
+    )
+
+    return(invisible())
+}
