e98e7de4a7a68830a6c40338aa255446fe197814
Mitchell Maier <mmaier@ucsb.edu>
2018-02-15 09:31:54 -0800

    gsub, one xls sheet
    Fix gsub command
    Fix return(invisible())
    Allow for different naming of xls with one sheet

--- R/excel_to_csv.R
+++ R/excel_to_csv.R
@@ -14,37 +14,40 @@
 #'
 #' @return (invisible())
 excel_to_csv <- function(path, directory = NULL, ...) {
-    # Stop if the user doesn't have the readxl package installed
-    if (!requireNamespace("readxl")) {
-        stop(call. = FALSE,
-             "The readxl package is required to convert excel workbooks to csv. Please install it and try again.")
-    }
-
-    stopifnot(file.exists(path))
-    if (!is.null(directory)) {
-        stopifnot(file.exists(directory))
-    } else {
-        directory = dirname(path)
-    }
-
-    excel_name <- basename(path)
-    excel_name <- gsub(".xls?$", "", excel_name, ignore.case = TRUE)
-
-    # Try to read excel file and split into csvs
-    tryCatch({
-        sheets <- excel_sheets(path)
-
-        lapply(seq_along(sheets), function(i) {
-            csv = read_excel(path, sheet = sheets[i], ...)
-
-            file_name <- paste0(sheets[i], "_", excel_name, ".csv")
-            file_path <- file.path(directory, file_name)
-
-            write.csv(csv, file_path , row.names = FALSE)})
-
-    },
-    error = function(e) {message("Error converting: ", path, " to csv\n")}
-    )
-
-    return(invisible())
+  # Stop if the user doesn't have the readxl package installed
+  if (!requireNamespace("readxl")) {
+    stop(call. = FALSE,
+         "The readxl package is required to convert excel workbooks to csv. Please install it and try again.")
+  }
+  
+  stopifnot(file.exists(path))
+  if (!is.null(directory)) {
+    stopifnot(file.exists(directory))
+  } else {
+    directory = dirname(path)
+  }
+  
+  excel_name <- basename(path)
+  excel_name <- gsub("\\.xls[x]?$", "", excel_name, ignore.case = TRUE)
+  
+  # Try to read excel file and split into csvs
+  tryCatch({
+    sheets <- readxl::excel_sheets(path)
+    
+    lapply(seq_along(sheets), function(i) {
+      csv = readxl::read_excel(path, sheet = sheets[i])
+      
+      if (length(sheets) == 1) {
+      file_name <- paste0(excel_name, ".csv")
+      } else {
+      file_name <- paste0(sheets[i], "_", excel_name, ".csv")}
+      file_path <- file.path(directory, file_name)
+      
+      write.csv(csv, file_path , row.names = FALSE)})
+    
+  },
+  error = function(e) {message("Error converting: ", path, " to csv\n")}
+  )
+  
+  invisible()
 }
