26671cc1d5519551bd77a2a44d31eb4315511e1c
Stephanie Freund <smfreu@gmail.com>
2018-02-20 14:34:55 -0800

    Corrected an error in the dummy data frame for enumerated data. Edited comments. Made function and test lines "tidier" with spaces etc. Function now performs basic editing capibility for all but enumerated domains. Not yet functional for custom units. User must still specify all of the arguments that occur within their data type (measurementScale) even if not intending to edit those arguments.

--- R/edit_attribute.R
+++ R/edit_attribute.R
@@ -25,7 +25,7 @@ attributes1 <- data.frame(
     missingValueCodeExplanation = c(NA,NA,NA,NA,NA),
     stringsAsFactors = FALSE)
 Levs <- c(A = 'high', B = 'medium', C = 'low')
-factors1 <- data.frame(attributeName = 'Levs', code = names(Levs), definition = unname(Levs))
+factors1 <- data.frame(attributeName = 'col3', code = names(Levs), definition = unname(Levs))
 factors1
 attributeList1 <- set_attributes(attributes1, factors=factors1)
 phys <- pid_to_eml_physical(mnTest, pkg$data[1])
@@ -53,40 +53,54 @@ eml@dataset@dataTable[[1]]@attributeList@attribute[[1]]@measurementScale@ratio@u
 eml@dataset@dataTable[[1]]@attributeList@attribute[[1]]@measurementScale@ratio
 
 #Function below includes only the required slots.
-#Problem 1: Currently only works for nominal/text data.
-#Problem 2: Fails if any arguments existing within a certain measurementScale are not specified
+#Fails if any arguments existing within a certain measurementScale are not specified
+#Currently works for all measurementTypes, but not functional yet for enumeratedDomain.
+#Not yet functional for custom units.
+
 #dataTable is the data table number in the data table list, attribute is the attribute number in the attribute list for that data table.
 
-edit_attribute <- function(eml, dataTable, attribute, attributeName=NA, attributeDefinition=NA, measurementScale=NA, unit=NA, numberType=NA,
-                           definition=NA, formatString=NA, missingValueCode=NA, missingValueCodeExplanation=NA){
+edit_attribute <- function(eml, dataTable, attribute, attributeName = NA, attributeDefinition = NA,
+                           measurementScale = NA, unit = NA, numberType = NA, definition = NA, formatString = NA,
+                           missingValueCode = NA, missingValueCodeExplanation = NA){
     eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@attributeName <- attributeName
     eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@attributeDefinition <- attributeDefinition
     if (measurementScale == 'nominal') {
-        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@nominal@nonNumericDomain@textDomain[[1]]@definition<-definition
+        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@nominal@nonNumericDomain@textDomain[[1]]@definition <- definition
     } else if (measurementScale == 'ordinal') {
-        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@ordinal
+        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@ordinal@nonNumericDomain@textDomain[[1]]@definition <- definition
     } else if (measurementScale == 'interval') {
-        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@interval@unit@standardUnit[[1]]<-unit
-        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@interval@numericDomain@numberType<-numberType
+        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@interval@unit@standardUnit[[1]] <- unit
+        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@interval@numericDomain@numberType[[1]] <- numberType
     } else if (measurementScale == 'ratio') {
-        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@ratio@unit@standardUnit[[1]]<-unit
-        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@ratio@numericDomain@numberType[[1]]<-numberType
+        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@ratio@unit@standardUnit[[1]] <- unit
+        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@ratio@numericDomain@numberType[[1]] <- numberType
     } else if (measurementScale == 'dateTime') {
-        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@dateTime@formatString<-formatString
+        eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@measurementScale@dateTime@formatString <- formatString
     }
-    eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@missingValueCode[[1]]@code<-missingValueCode
-    eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@missingValueCode[[1]]@codeExplanation<-missingValueCodeExplanation
+    eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@missingValueCode[[1]]@code <- missingValueCode
+    eml@dataset@dataTable[[dataTable]]@attributeList@attribute[[attribute]]@missingValueCode[[1]]@codeExplanation <- missingValueCodeExplanation
 
     return(eml)
 }
 
-#This works:
-eml2<-edit_attribute(eml,1, 2, attributeName="TestNominal", attributeDefinition="trying out if function works", measurementScale="nominal",
-               definition="trying out if function works",missingValueCode='NA', missingValueCodeExplanation='data not available')
+#Tests
+eml2 <- edit_attribute(eml, 1, 2, attributeName = 'TestNominal', attributeDefinition = 'trying out if function works', measurementScale = 'nominal',
+               definition = 'trying out if function works', missingValueCode = 'NA', missingValueCodeExplanation = 'data not available')
 
-#This fails on the unit
-eml3<-edit_attribute(eml,1, 1, attributeName="TestRatio", attributeDefinition="trying out if function works", measurementScale='ratio',
-                     unit='dimensionless', numberType='integer',missingValueCode='NA', missingValueCodeExplanation='data not available')
+eml3 <- edit_attribute(eml, 1, 1, attributeName = 'TestRatio', attributeDefinition = 'trying out if function works', measurementScale = 'ratio',
+                     unit = 'dimensionless', numberType = 'integer', missingValueCode = 'NA', missingValueCodeExplanation = 'data not available')
 
+eml4 <- edit_attribute(eml, 1, 4, attributeName = 'TestInterval', attributeDefinition = 'trying out if function works', measurementScale = 'interval',
+                     unit = 'dimensionless', numberType = 'whole', missingValueCode = 'NA', missingValueCodeExplanation = 'data not available')
+
+eml5 <- edit_attribute(eml, 1, 5, attributeName = 'TestDate', attributeDefinition = 'trying out if function works', measurementScale = 'dateTime',
+                     formatString = 'YYYY-MM', missingValueCode = 'NA', missingValueCodeExplanation = 'data not available')
+
+eml6 <- edit_attribute(eml, 1, 3, attributeName = 'TestOrdinal', attributeDefinition = 'trying out if function works', measurementScale = 'ordinal',
+                     definition = 'trying out if function works', missingValueCode = 'NA', missingValueCodeExplanation = 'data not available')
+
+eml2@dataset@dataTable[[1]]
 eml3@dataset@dataTable[[1]]
-class(eml@dataset@dataTable[[1]]@attributeList@attribute[[1]]@measurementScale@ratio@unit@standardUnit[[1]])
+eml4@dataset@dataTable[[1]]
+eml5@dataset@dataTable[[1]]
+eml6@dataset@dataTable[[1]]
