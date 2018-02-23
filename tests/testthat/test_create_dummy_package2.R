6dced7a579dcb7b3b2216351d6f6f0b8d8d0c663
Irene <25118334+isteves@users.noreply.github.com>
2018-02-22 16:34:34 -0800

    added skip if no tokens are set

--- tests/testthat/test_create_dummy_package2.R
+++ tests/testthat/test_create_dummy_package2.R
@@ -10,6 +10,10 @@ cnTest <- dataone::CNode('STAGING')
 mnTest <- dataone::getMNode(cnTest,'urn:node:mnTestARCTIC')
 
 test_that("Errors work", {
+    if (!is_token_set(mnTest)) {
+        skip("No token set. Skipping test.")
+    }
+
     expect_error(create_dummy_package2(mnTest, title = 11))
     expect_error(create_dummy_package2("mnTest"))
 })
