dbab65b51b16938d2ca881c7ab17794d40c31fb5
maier-m <mmaier@ucsb.edu>
2018-02-28 09:33:57 -0800

    Fixes to dimensionless

--- tests/testthat/test_get_custom_units.R
+++ tests/testthat/test_get_custom_units.R
@@ -3,10 +3,10 @@ testthat::context("Get Custom Units")
 testthat::test_that("get_custom_units accepts multiple unit input formats", {
 
     form_1 <- datamgmt::get_custom_units('kilometersPerSquareSecond') #preferred input form
-    form_2 <- get_custom_units('Kilometers per seconds squared')
-    form_3 <- get_custom_units('km/s^2')
-    form_4 <- get_custom_units('km s-2')
-    form_5 <- get_custom_units('s-2 /     kilometers-1')
+    form_2 <- datamgmt::get_custom_units('Kilometers per seconds squared')
+    form_3 <- datamgmt::get_custom_units('km/s^2')
+    form_4 <- datamgmt::get_custom_units('km s-2')
+    form_5 <- datamgmt::get_custom_units('s-2 /     kilometers-1')
 
     forms_equal <- (form_1 == form_2 &&
                     form_1 == form_3 &&
@@ -16,11 +16,9 @@ testthat::test_that("get_custom_units accepts multiple unit input formats", {
     testthat::expect_true(forms_equal)
 })
 
-testthat::test_that("get_custom_units recognizes reciprocal meters", {
+testthat::test_that("get_custom_units recognizes reciprocals", {
 
-    out_1 <- datamgmt::get_custom_units('per meter') #preferred input form
-    out_2 <- datamgmt::get_custom_units('radians per meter') #preferred input form
+    out_1 <- datamgmt::get_custom_units('per meter')
 
     testthat::expect_true(out_1$unitType == "lengthReciprocal")
-    testthat::expect_true(out_2$unitType == "lengthReciprocal")
 })
