c8997d540f3fadf227c1fc393d334110a22a539f
Irene <25118334+isteves@users.noreply.github.com>
2018-02-27 17:19:42 -0800

    fixed bug (forgot `any` in update_physical)

--- R/update_package_object.R
+++ R/update_package_object.R
@@ -17,14 +17,14 @@ update_physical <- function(eml,
                             data_pid,
                             new_data_pid) {
 
-    if (!requireNamespace("stringr")) {
-        stop(call. = FALSE,
-             "The stringr package is required. Please install it and try again.")
+    all_url <- unlist(EML::eml_get(eml, "url"))
+    if(sum(stringr::str_detect(all_url, data_pid)) == 0){
+        stop("The data pid does not match any physical sections. The EML will not be updated.")
     }
 
     dataTable_url <- unlist(EML::eml_get(eml@dataset@dataTable, "url"))
 
-    if(!is.null(dataTable_url) && stringr::str_detect(dataTable_url, data_pid)){
+    if(!is.null(dataTable_url) && any(stringr::str_detect(dataTable_url, data_pid))){
         position <- which(stringr::str_detect(dataTable_url, data_pid))
         new_phys <- arcticdatautils::pid_to_eml_physical(mn, new_data_pid)
         eml@dataset@dataTable[[position]]@physical@.Data <- new_phys
@@ -32,10 +32,10 @@ update_physical <- function(eml,
 
     otherEntity_url <- unlist(EML::eml_get(eml@dataset@otherEntity, "url"))
 
-    if(!is.null(dataTable_url) && stringr::str_detect(otherEntity_url, data_pid)){
+    if(!is.null(dataTable_url) && any(stringr::str_detect(otherEntity_url, data_pid))){
         position <- which(stringr::str_detect(otherEntity_url, data_pid))
         new_phys <- arcticdatautils::pid_to_eml_other_entity(mn, new_data_pid)
-        eml@dataset@otherEntity[[1]] <- new_phys
+        eml@dataset@otherEntity[[position]] <- new_phys
     }
 
     invisible(eml)
@@ -45,15 +45,15 @@ update_physical <- function(eml,
 #'
 #' This function updates an data object, and then automatically
 #' updates the package resource map with the new data pid. It
-#' is a convenience wrapper around \link[arcticdatautils]{update_object}}
-#' and \link[arcticdatautils]{update_resource_map}}.
+#' is a convenience wrapper around \link[arcticdatautils]{update_object}
+#' and \link[arcticdatautils]{update_resource_map}.
 #'
 #' @param mn (MNode) Member node
 #' @param data_pid (character) PID for data object to update
 #' @param new_data_path (character) Path to new data object
 #' @param resource_map_pid (character) PID for resource map to update
 #' @param format_id (character) Optional. The format ID to set for the object.
-#' When not set, \link[arcticdatautils]{guess_format_id}} will be used
+#' When not set, \link[arcticdatautils]{guess_format_id} will be used
 #' to guess the format ID. Should be a \href{https://cn.dataone.org/cn/v2/formats}{DataONE format ID}.
 #' @param public (logical) Optional. Make the update public. If FALSE,
 #' will set the metadata and resource map to private (but not the data objects).
@@ -72,6 +72,9 @@ update_package_object <- function(mn,
                                   data_pid,
                                   new_data_path,
                                   resource_map_pid,
+                                  #alternative to only rm pid: (saves on time needed for get_package)
+                                  # metadata_pid,
+                                  # data_pids,
                                   format_id = NULL,
                                   public = FALSE,
                                   use_doi = FALSE) {
@@ -84,11 +87,16 @@ update_package_object <- function(mn,
     stopifnot(is.character(resource_map_pid))
     stopifnot(is.logical(public))
 
-    pkg <- arcticdatautils::get_package(mn, resource_map_pid)
+    pkg <- get_package(mnTest, resource_map_pid)
+    #if metadata and data pids are provided, can skip get_package and use this:
+    # pkg <- list(metadata = metadata_pid,
+    #             resource_map = resource_map_pid,
+    #             data = data_pids)
+    eml <- EML::read_eml(rawToChar(dataone::getObject(mn, pkg$metadata)))
 
     new_data_pid <- arcticdatautils::update_object(mn,
-                                                   data_pid,
-                                                   new_data_path,
+                                                   pid = data_pid,
+                                                   path = new_data_path,
                                                    format_id = format_id)
 
     cat("The new data pid is", new_data_pid)
@@ -98,23 +106,31 @@ update_package_object <- function(mn,
     new_data_pids <- c(other_data_pids, new_data_pid)
 
     #update EML
-    eml <- EML::read_eml(rawToChar(dataone::getObject(mn, pkg$metadata)))
+    eml_new <- tryCatch(update_physical(eml = eml,
+                                    mn = mn,
+                                    data_pid = data_pid,
+                                    new_data_pid = new_data_pid),
+                    error = function(e) {
+                        print("The data pid does not match any physical sections. The EML will not be updated.")
+                        return(eml)
+                    })
 
-    eml <- update_physical(eml = eml,
-                           mn = mn,
-                           data_pid = data_pid,
-                           new_data_pid = new_data_pid)
+    eml_path <- "science-metadata.xml"
+    file.create(eml_path)
+    EML::write_eml(eml_new, eml_path)
 
-    eml_path <- tempfile(fileext = ".xml")
-    write_eml(eml, eml_path)
+    print(unlist(eml_get(eml, "url")))
+    print(unlist(eml_get(eml_new, "url")))
 
     pkg_new <- publish_update(mn,
                               metadata_pid = pkg$metadata,
                               resource_map_pid = pkg$resource_map,
                               metadata_path = eml_path,
-                              data_pid = new_data_pids,
+                              data_pids = new_data_pids,
                               use_doi = use_doi,
                               public = public)
+
+    file.remove(eml_path)
 
     return(pkg_new)
 }
