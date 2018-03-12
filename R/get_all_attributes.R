c40db4137595735895debd5bd4389df0bbc84876
Dominic Mullen <dmullen17@gmail.com>
2018-03-12 13:56:18 -0700

    updated function

--- R/get_all_attributes.R
+++ R/get_all_attributes.R
@@ -1,69 +1,55 @@
 #' Get all attribute from an EML document.  Document for the function \code{get_attributes}
 #' from the EML(url) Package
 #'
-#' @param eml (S4) Optional. The full eml document. Required if \code{url} is not specified.
-#' @param url (character) Optional. The url to the Dataone Package. Required if \code{eml} is not specified.
-#' @param node (MNode/CNode) Optional.  Defaults to Arctic Data Center Production node.
+#'@param node (MNode/CNode) The Dataone Node that stores the Metadata object.
+#' @param eml (S4 / character) Optional. Either the full eml S4 object document or the url of the Dataone Package.
+#' The S4 object input is a more reliable method.
 #' @return (list) A list of all attribute metadata from the EML in data.frame objects
 #'
 #' @export
 #'
-#' @author Dominic Mullen
+#' @author Dominic Mullen, \email{dmullen17@@gmail.com}
 #'
 #' @examples
 #' \dontrun{
-#' install_github("ropensci/EML)
+#' devtools::install_github("ropensci/EML")
 #' library(EML)
 #' library(dataone)
 #' cn <- dataone::CNode('PROD')
 #' node <- dataone::getMNode(cn,'urn:node:ARCTIC')
 #' eml <- EML::read_eml(rawToChar(dataone::getObject(node, "doi:10.18739/A23W02")))
-#' attributes <- datamgmt::get_all_attributes(eml)
+#' attributes <- datamgmt::get_attributes_url(eml)
 #'
-#' attributes <- datamgmt::get_all_attributes(link = "https://arcticdata.io/catalog/#view/doi:10.18739/A23W02")
+#' attributes <- datamgmt::get_attributes_url(node, "https://arcticdata.io/catalog/#view/doi:10.18739/A23W02")
 #'
 #' # switch nodes
 #' cn <- dataone::CNode('PROD')
 #' knb <- dataone::getMNode(cn,"urn:node:KNB")
-#' attributes <- get_all_attributes(url = "https://knb.ecoinformatics.org/#view/doi:10.5063/F1639MWV",
-#'                                  node = knb)
+#' attributes <- get_attributes_url(node = knb,
+#'                                  url = "https://knb.ecoinformatics.org/#view/doi:10.5063/F1639MWV")
 #' }
-get_all_attributes <- function(eml = NULL, url = NULL, node = NULL) {
-    if (is.null(eml) & is.null(url)) {
-        stop("One of the arguments 'eml' or 'url' must be specified.")
-    }
-    if (!is.null(eml)) {
-        stopifnot(isS4(eml))
-    }
+get_attributes_url <- function(node, eml) {
+    # TODO - load RData objects
+    # TODO - write to individual csvs
+    # TODO - write to one excel workbook
+    # TODO - make sure it works for otherEntities
+    stopifnot(methods::is(node, "MNode") || is(node, "CNode"))
+    stopifnot(any(isS4(eml), is.character(eml)))
 
-    # If node is specified run checks, otherwise set node = ADC production
-    if (!is.null(node)) {
-        stopifnot(methods::is(node, "MNode") || is(node, "CNode"))
-    } else {
-        cn <- dataone::CNode('PROD')
-        node <- dataone::getMNode(cn,'urn:node:ARCTIC')
-    }
-
-    # If url is specified extract pid and download eml
-    if (!is.null(url)) {
-        stopifnot(is.character(url))
-
-        pid <- unlist(strsplit(url, "view/"))[[2]]
+    # If url input is specified extract pid and download eml
+    if (is.character(eml)) {
+        pid <- unlist(strsplit(eml, "view/"))[[2]]
         eml <- EML::read_eml(rawToChar(dataone::getObject(node, pid)))
     }
 
-    n <- length(eml@dataset@dataTable)
-    if (n == 0){
+    n1 <- length(eml@dataset@dataTable)
+    n2 <- length(eml@dataset@otherEntity)
+    if (n1 == 0 & n2 == 0){
         stop("EML does not contain attribute metadata")
     }
 
-    results <- lapply(eml@dataset@dataTable, function(dataTable) {
-        att <- EML::eml_get(dataTable, "attributeList", "entityName")
-    })
-
-    names(results) <- lapply(eml@dataset@dataTable, function(dataTable) {
-        EML::eml_get(dataTable, "entityName")
-    })
+    results <- EML::eml_get(eml, "attributeList")
+    names(results) <- EML::eml_get(eml, "entityName")
 
     return(results)
 }
