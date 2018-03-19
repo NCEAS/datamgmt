c7720454452fe4127e63802f2539814c5769dbc6
Stephanie Freund <smfreu@gmail.com>
2018-03-19 10:47:46 -0700

    Removed my old comments and added roxygen text: title, description, arguments, examples and export

--- R/edit_attribute.R
+++ R/edit_attribute.R
@@ -1,21 +1,29 @@
-##Function to edit an attribute in an existing attributes table.
+#'Edit a single attribute.
+#'
+#'This function edits the slots of a single attribute in an existing attribute table.
+#'
+#'Can only be used on attributes entirely defined within the "attributes" slot of attributeList; it cannot be used to edit the factor table of an enumeratedDomain.
+#'In cases with very large attribute lists, user may want to use which_in_eml function first to locate the attribute position.
+#'
+#'@param eml The eml object containing the attributeList
+#'@param dataTableNumber The number of the dataTable containing the attributeList; in other words its position in the dataTable list.
+#'@param attributeNumber The number of the attribute of interest; in other words its position in the attributeList.
+#'@param attributeName... Arguments for specifying new values of attribute slots.
+#'
+#'@examples
+#' \dontrun{
+#' #To change an attribute's name and add a missing value code where there hadn't been one:
+#' new_eml <- edit_attribute(eml, 2, 8, attributeName = "new name", missingValueCode = "NA", missingValueCodeExplanation = "Data unavailable")
+#' #To change an attribute's attributeDefinition, as well as change measurementScale from nominal to ratio (requires also changing domain, unit, and numberType, as well as setting definition to NA):
+#' new_eml <- edit_attribute(eml, 1, 2, attributeDefinition = "new definition", domain = "numericDomain",
+#' measurementScale = "ratio", unit = "dimensionless", numberType = "whole", definition = NA)
+#' }
+#'
+#'@export
 
-# for now:
-#devtools::install_github("ropensci/EML", force = TRUE)
 
+devtools::install_github("ropensci/EML", force = TRUE)
 
-#Function below includes only required slots.
-#Need to add checks to ensure that fields match measurementScale (e.g. if measurementScale = ratio, unit =/= NULL and definiton must be NA)
-#Also does not currently include a way to change the codes in the factor table for an enumerated domain; may need to be its own function?
-
-#User needs to:
-###specify the eml object in the argument "eml"
-###specify which dataTable (by position) in the argument "dataTableNumber"
-###specify position of attribute in attributeList for argument "attributeNumber"
-###In cases with large attribute lists, user may want to use which_element function to locate the attribute position.
-
-
-##Trying out with additional statements for NULL
 edit_attribute <- function(eml, dataTableNumber, attributeNumber, attributeName = NULL, attributeDefinition = NULL, domain=NULL,
                            measurementScale = NULL, unit = NULL, numberType = NULL, definition = NULL, formatString = NULL,
                            missingValueCode = NULL, missingValueCodeExplanation = NULL){
