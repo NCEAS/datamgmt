06c897744e020c42d45a0775981a4426e67aba66
Stephanie Freund <smfreu@gmail.com>
2018-03-20 14:30:57 -0700

    Edited for style and added link to which_in_eml into documentation.

--- R/edit_attribute.R
+++ R/edit_attribute.R
@@ -3,7 +3,7 @@
 #' This function edits the slots of a single attribute in an existing attribute table.
 #'
 #' Can only be used on attributes entirely defined within the "attributes" slot of attributeList; it cannot be used to edit the factor table of an enumeratedDomain.
-#' In cases with very large attribute lists, user may want to use which_in_eml function first to locate the attribute position.
+#' In cases with very large attribute lists, user may want to use \link[datamgmt]{which_in_eml} function first to locate the attribute position.
 #'
 #' @param eml The eml object containing the attributeList
 #' @param dataTableNumber The number of the dataTable containing the attributeList; in other words its position in the dataTable list.
@@ -24,8 +24,10 @@
 #' @examples
 #' \dontrun{
 #' #To change an attribute's name and add a missing value code where there hadn't been one:
-#' new_eml <- edit_attribute(eml, 2, 8, attributeName = "new name", missingValueCode = "NA", missingValueCodeExplanation = "Data unavailable")
-#' #To change an attribute's attributeDefinition, as well as change measurementScale from nominal to ratio (requires also changing domain, unit, and numberType, as well as setting definition to NA):
+#' new_eml <- edit_attribute(eml, 2, 8, attributeName = "new name", missingValueCode = "NA",
+#'                           missingValueCodeExplanation = "Data unavailable")
+#' #To change an attribute's attributeDefinition, as well as change measurementScale from nominal to ratio
+#' #(requires also changing domain, unit, and numberType, as well as setting definition to NA):
 #' new_eml <- edit_attribute(eml, 1, 2, attributeDefinition = "new definition", domain = "numericDomain",
 #' measurementScale = "ratio", unit = "dimensionless", numberType = "whole", definition = NA)
 #' }
@@ -35,42 +37,42 @@ edit_attribute <- function(eml, dataTableNumber, attributeNumber, attributeName 
                            measurementScale = NULL, unit = NULL, numberType = NULL, definition = NULL, formatString = NULL,
                            missingValueCode = NULL, missingValueCodeExplanation = NULL){
 
-    data<-EML::get_attributes(eml@dataset@dataTable[[dataTableNumber]]@attributeList)
-    attributeTable<-data.frame(data$attributes) #this excludes the factor table from enumerated domain.
+    data <- EML::get_attributes(eml@dataset@dataTable[[dataTableNumber]]@attributeList)
+    attributeTable <- data.frame(data$attributes) #this excludes the factor table from enumerated domain.
 
-    if(!is.null(attributeName)==TRUE){
-        attributeTable[attributeNumber,1] <- attributeName
+    if(!is.null(attributeName)) {
+        attributeTable[attributeNumber, 1] <- attributeName
     }
-    if(!is.null(attributeDefinition)==TRUE){
-        attributeTable[attributeNumber,17] <- attributeDefinition
+    if(!is.null(attributeDefinition)) {
+        attributeTable[attributeNumber, 17] <- attributeDefinition
     }
-    if(!is.null(measurementScale)==TRUE){
-        attributeTable[attributeNumber,16] <- measurementScale
+    if(!is.null(measurementScale)) {
+        attributeTable[attributeNumber, 16] <- measurementScale
     }
-    if(!is.null(domain)==TRUE){
-        attributeTable[attributeNumber,2] <- domain
+    if(!is.null(domain)) {
+        attributeTable[attributeNumber, 2] <- domain
     }
-    if(!is.null(unit)==TRUE){
-        attributeTable[attributeNumber,6] <- unit
+    if(!is.null(unit)) {
+        attributeTable[attributeNumber, 6] <- unit
     }
-    if(!is.null(numberType)==TRUE){
-        attributeTable[attributeNumber,7] <- numberType
+    if(!is.null(numberType)) {
+        attributeTable[attributeNumber, 7] <- numberType
     }
-    if(!is.null(definition)==TRUE){
-        attributeTable[attributeNumber,9] <- definition
+    if(!is.null(definition)) {
+        attributeTable[attributeNumber, 9] <- definition
     }
-    if(!is.null(formatString)==TRUE){
-        attributeTable[attributeNumber,8] <- formatString
+    if(!is.null(formatString)) {
+        attributeTable[attributeNumber, 8] <- formatString
     }
-    if(!is.null(missingValueCode)==TRUE){
-        attributeTable[attributeNumber,14] <- missingValueCode
+    if(!is.null(missingValueCode)) {
+        attributeTable[attributeNumber, 14] <- missingValueCode
     }
-    if(!is.null(missingValueCodeExplanation)==TRUE){
-        attributeTable[attributeNumber,15] <- missingValueCodeExplanation
+    if(!is.null(missingValueCodeExplanation)) {
+        attributeTable[attributeNumber, 15] <- missingValueCodeExplanation
     }
 
-    attribute.list<-EML::set_attributes(attributeTable, factors = data$factors)
-    eml@dataset@dataTable[[dataTableNumber]]@attributeList <- attribute.list
+    attribute_list <- EML::set_attributes(attributeTable, factors = data$factors)
+    eml@dataset@dataTable[[dataTableNumber]]@attributeList <- attribute_list
     return(eml)
     EML:::check_and_complete_attributes(attributeTable, NULL)
 
