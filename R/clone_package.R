aa25b19bbdaa85fe15cfeb29a90f58fd763ed5f8
Dominic Mullen <dmullen17@gmail.com>
2018-03-21 14:31:43 -0700

    combined clone_package and clone_packages into one

--- R/clone_package.R
+++ R/clone_package.R
@@ -62,6 +62,7 @@ get_eml_pids <- function(eml) {
 
 #' Clone a Data Package between Member Nodes without its child packages.
 #'
+#' @description The datamgmt wrapper function `clone_package` should be used instead.
 #' This function copies a Data Package from one DataOne member node to another.
 #' It can also be used to copy an older version of a Data Package to the same
 #' member node in order to restore it, provided that the old Package is then
@@ -82,10 +83,8 @@ get_eml_pids <- function(eml) {
 #' clone_package("resource_map_doi:10.18739/A2RZ6X", from, to)
 #' }
 #'
-#' @export
-#'
 #' @return (list) List of all the identifiers in the new Data Package.
-clone_package <- function(resource_map_pid, from, to) {
+clone_one_package <- function(resource_map_pid, from, to) {
     #' TODO switch remaining for loops to applys
     #' TODO add more messages
     stopifnot(is.character(resource_map_pid))
@@ -100,7 +99,8 @@ clone_package <- function(resource_map_pid, from, to) {
         data_pids <- package$data
     }
 
-    response <- vector("list", length = 3)
+    # Prepare the response object
+    response <- list()
 
     # Solr query data formatId, fileName, and identifier
     solr_query <- get_object_metadata(from, resource_map_pid)
@@ -165,22 +165,21 @@ clone_package <- function(resource_map_pid, from, to) {
     }
 
     response[["resource_map"]] <- new_resource_map_pid
-    names(response) <- c("data", "metadata", "resource_map")
 
     return(response)
 }
 
-#' Clone a Data Package between Member Nodes with its child packages.
+#' Clone a Data Package between Member Nodes without its child packages.
 #'
-#' This function copies a Data Package, including its child packages,
-#' from one DataOne member node to another. It can also be used to copy an older
-#' version of a Data Package to the same member node in order to restore it, provided
-#' that the old Package is then obsoleted by the copied version.  This will not
-#' update the pids in the metadata.
+#' This function copies a Data Package from one DataOne member node to another.
+#' It can also be used to copy an older version of a Data Package to the same
+#' member node in order to restore it, provided that the old Package is then
+#' obsoleted by the copied version.  This will not update the pids in the metadata.
 #'
 #' @param resource_map_pid (chraracter) The identifier of the Resource Map for the package to download.
 #' @param from (MNode) The Member Node to download from.
 #' @param to (MNode) The Member Node to upload to.
+#' @param clone_child_packages (logical) Whether or not to clone the child packages.  Defaults to \code{FALSE}
 #'
 #' @author Dominic Mullen, \email{dmullen17@@gmail.com}
 #'
@@ -194,7 +193,10 @@ clone_package <- function(resource_map_pid, from, to) {
 #' }
 #'
 #' @export
-clone_packages <- function(resource_map_pid, from, to) {
+clone_package <- function(resource_map_pid,
+                          from,
+                          to,
+                          clone_child_packages = FALSE) {
     #' TODO - create dynamic structure that allows for more than one level of
     #' children (3+ nesting levels)
     #' TODO - add messages per child package?
@@ -202,30 +204,39 @@ clone_packages <- function(resource_map_pid, from, to) {
     #' 3) copy_package
     #' TODO - query all pids for unique rightsHolders and add to Sysmeta
     # Clone initial package without children
-    package <- clone_one_package(mn_pull, mn_push, resource_map_pid)
+    stopifnot(is.logical(clone_child_packages))
 
-    if (length(package$child_packages) != 0) {
+    # Get package information
+    package <- arcticdatautils::get_package(from, resource_map_pid)
 
+    # Clone the parent package
+    response <- clone_one_package(resource_map_pid, from, to)
+
+    if (clone_child_packages == TRUE) {
         n_child_packages <- length(package$child_packages)
 
-        # Clone child packages
-        child_packages <- unlist(lapply(seq_len(n_child_packages), function(i) {
-            clone_one_package(mn_pull, mn_push, package$child_packages[i])
-        }))
+        if (n_child_packages > 0) {
 
-        # Select resource_map_pid(s) of child packages
-        indices <- which(names(child_packages) == "resource_map")
-        child_resource_map_pids <- child_packages[indices]
+            # Clone child packages
+            child_packages <- unlist(lapply(seq_len(n_child_packages), function(i) {
+                clone_one_package(package$child_packages[i], from, to)
+            }))
 
-        # Nest child packages
-        updated_resource_map_pid <- arcticdatautils::update_resource_map(mn_push,
-                                                                         package$resource_map,
-                                                                         package$metadata,
-                                                                         package$data,
-                                                                         child_resource_map_pids)
+            # Select resource_map_pid(s) of child packages
+            indices <- which(names(child_packages) == "resource_map")
+            child_resource_map_pids <- child_packages[indices]
 
-        package[["resource_map"]] = updated_resource_map_pid
+            # Nest child packages
+            updated_resource_map_pid <- arcticdatautils::update_resource_map(to,
+                                                                             response$resource_map,
+                                                                             response$metadata,
+                                                                             response$data,
+                                                                             child_resource_map_pids)
+
+            response[["resource_map"]] <- updated_resource_map_pid
+            response[["child_packages"]] <- unname(child_resource_map_pids)
+        }
     }
 
-    return(package)
+    return(response)
 }
--- tests/testthat/test_clone_package.R
+++ tests/testthat/test_clone_package.R
@@ -1,44 +1,102 @@
 context("test_clone_package")
 
+# Set test node
 cn <- dataone::CNode('STAGING')
 mn_test <- dataone::getMNode(cn,'urn:node:mnTestARCTIC')
 
 test_that("clone_package errors gracefully", {
     expect_error(clone_package(5, mn_test, mn_test))
+
     expect_error(clone_package("resource_map_doi:10.18739/A2RZ6X",
                  "Dummy Node",
                  mn_test))
+
     expect_error(clone_package("resource_map_doi:10.18739/A2RZ6X",
                                mn_test))
+
     suppressWarnings(expect_error(clone_package("Dummy pid", mn_test, mn_test)))
+
+    expect_error(clone_package("resource_map_doi:10.18739/A2RZ6X",
+                               mn_test,
+                               mn_test,
+                               "Dummy Value"))
 })
 
 test_that("clone_package copies a package with no data pids", {
-    if (!is_token_set(mn_test)) {
+    if (!arcticdatautils::is_token_set(mn_test)) {
         skip("No token set. Skipping test.")
     }
 
     # Make a test package
-    package <- create_dummy_package(mn_test, size = 1)
+    package <- arcticdatautils::create_dummy_package(mn_test, size = 1)
     expect_named(package, c("metadata", "resource_map", "data"))
 
     # Clone package
     new_pids <- clone_package(package$resource_map, mn_test, mn_test)
+
+    # Package tests
     expect_named(new_pids, c("data", "metadata", "resource_map"))
+    expect_true(all(arcticdatautils::object_exists(mn_test, unlist(new_pids))))
+    expect_length(new_pids, 3)
+
+    # Check object lengths
+    lengths <- sapply(new_pids, length)
+    expect_equivalent(lengths, c(0,1,1))
 })
 
 test_that("clone_package copies a package", {
-    if (!is_token_set(mnTest)) {
+    if (!arcticdatautils::is_token_set(mn_test)) {
         skip("No token set. Skipping test.")
     }
 
     # Make a test package
-    package <- create_dummy_package(mnTest)
+    package <- arcticdatautils::create_dummy_package(mn_test)
     expect_named(package, c("metadata", "resource_map", "data"))
 
     # Clone package
-    new_pids <- clone_package(package$resource_map, mnTest, mnTest)
+    new_pids <- clone_package(package$resource_map, mn_test, mn_test)
 
+    # Package tests
     expect_named(new_pids, c("data", "metadata", "resource_map"))
-    expect_true(all(arcticdatautils::object_exists(mnTest, unlist(new_pids))))
+    expect_true(all(arcticdatautils::object_exists(mn_test, unlist(new_pids))))
+    expect_length(new_pids, 3)
+
+    # Check object lengths
+    lengths <- sapply(new_pids, length)
+    expect_equivalent(lengths, c(1,1,1))
+})
+
+test_that("clone_package copies a package with a child package", {
+    if (!arcticdatautils::is_token_set(mn_test)) {
+        skip("No token set. Skipping test.")
+    }
+
+    # Make a test parent package
+    parent <- arcticdatautils::create_dummy_package(mn_test)
+    expect_named(parent, c("metadata", "resource_map", "data"))
+
+    # Make a test child package
+    child <- arcticdatautils::create_dummy_package(mn_test)
+    expect_named(child, c("metadata", "resource_map", "data"))
+
+    # Nest packages
+    rm <- arcticdatautils::update_resource_map(mn_test,
+                                               parent$resource_map,
+                                               parent$metadata,
+                                               parent$data,
+                                               child$resource_map,
+                                               check_first = FALSE)
+
+    # Clone package
+    new_pids <- clone_package(rm, mn_test, mn_test, clone_child_packages = TRUE)
+
+    expect_named(new_pids, c("data", "metadata", "resource_map", "child_packages"))
+    expect_true(all(arcticdatautils::object_exists(mn_test, unlist(new_pids))))
+    expect_length(new_pids, 4)
+
+    child <- arcticdatautils::get_package(mn_test, new_pids$child_packages)
+
+    # Check object lengths
+    lengths <- sapply(child, length)
+    expect_equivalent(lengths, c(1,1,1, 0))
 })
