8b096f4644fcbe6b90d54adc26a34be2290efe97
Dominic Mullen <dmullen17@gmail.com>
2018-03-26 15:56:37 -0700

    modularizing download_package

--- R/download_package.R
+++ R/download_package.R
@@ -32,12 +32,6 @@ remove_special_characters <- function(pid) {
 excel_to_csv_prefix <- function(path, prefix = NULL) {
     stopifnot(file.exists(path))
 
-    # Stop if the user doesn't have the readxl package installed
-    if (!requireNamespace("readxl")) {
-        stop(call. = FALSE,
-             "The readxl package is required to convert excel workbooks to csv. Please install it and try again.")
-    }
-
     # Try to read excel file and split into csvs
     tryCatch({
         sheets <- excel_sheets(path)
@@ -138,7 +132,6 @@ get_package_size <- function(mn, resource_map_pid, formatType = "*") {
 #' This is a helper function for 'download_package'
 #'
 #' @param download_size (numeric) Total size in bytes
-#'
 convert_bytes <- function(download_size) {
     #' TODO - make this function more robust using gdata::humanReadable as a template
     stopifnot(is.numeric(download_size))
@@ -160,6 +153,124 @@ convert_bytes <- function(download_size) {
     return(paste0(download_size, " ", unit))
 }
 
+#' Download multiple data objects using their pids.
+#'
+#' @description Download mutiple dataone objects.  This is a helper function
+#' for 'datamgmt::download_package'
+#' @param mn (MNode) The Dataone Member Node to download the data objects from.
+#' @param data_pids (character) A vector of Data object pids.
+#' @param out_paths (character) A vector of file paths to download to.
+#' @param n_max (numeric) Optional.  Number of attempts at downloading a Data object.
+download_data_objects <- function(mn, data_pids, out_paths, n_max = 3) {
+    stopifnot(methods::is(mn, "MNode"))
+    stopifnot(is.character(data_pids))
+
+    lapply(seq_len(data_pids), function(i) {
+
+        if (file.exists(out_paths[i])) {
+            warning(call. = FALSE,
+                    paste0("The file ", out_paths[i], " already exists. Skipping download."))
+        } else {
+            n_tries <- 0
+            dataObj <- "error"
+
+            while (n_tries < n_max & dataObj[1] == "download_error") {
+                dataObj <- tryCatch({
+                    dataone::getObject(mn, data_pids[i])
+                    writeBin(dataObj, out_paths[i])
+                },
+                error = function(e) {return("error")})
+                ntries <- ntries + 1
+            }
+        }
+    })
+
+    return(invisible())
+}
+
+#' Download one Package without its child Packages.
+#'
+#' This function downloads all of the Data Objects in a Data Package to the local filesystem.
+#' It is particularly useful when a Data Package is too large to download using the web interface.
+#'
+#' Setting \code{check_download_size} to \code{TRUE} is recommended if you are uncertain of the total download size and want to avoid downloading very large Data Packages.
+#'
+#' This function will also download any data objects it finds in any child Data Packages of the input data package.
+#' If you would only like to download data from one Data Package, set \code{download_child_packages} to \code{FALSE}.
+#'
+#' @param mn (MNode) The Member Node to download from.
+#' @param resource_map_pid (chraracter) The identifier of the Resource Map for the package to download.
+#' @param download_directory (character) The path of the directory to download the package to.
+#' @param prefix_file_names (logical) Optional.  Whether to prefix file names with the package metadata identifier.  This is useful when downloading files from multiple packages to one directory.
+#' @param convert_excel_to_csv (logical) Optional. Whether to convert excel files to csv(s).  The csv files are downloaded as sheetName_excelWorkbookName.csv
+#' @param download_column_metadata (logical) Optional.  Whether to download attribute (column) metadata as csvs.  If using this its recommened to also set \code{prefix_file_names = TRUE}
+#' @param convert_excel_to_csv (logical) Optional. Whether to convert excel files to csv(s).  This is not recommended if the separate csv files already exist in the package. The csv files are downloaded as sheetName_excelWorkbookName.csv
+#'
+#' @importFrom utils setTxtProgressBar txtProgressBar write.csv
+#'
+#' @author Dominic Mullen, \email{dmullen17@@gmail.com}
+#'
+download_one_package <- function(mn,
+                                 resource_map_pid,
+                                 download_directory,
+                                 prefix_file_names = TRUE,
+                                 download_column_metadata = FALSE,
+                                 convert_excel_to_csv = FALSE) {
+    # TODO Add option for downloading in folder structure that mirrors nesting
+    # rather than only all files in one folder
+    # TODO Convert check download size to helper function?
+
+    # Check that input arguments are in the correct format
+    stopifnot(methods::is(mn, "MNode"))
+    stopifnot(is.character(resource_map_pid))
+    stopifnot(file.exists(download_directory))
+    stopifnot(is.logical(convert_excel_to_csv))
+    if (convert_excel_to_csv == TRUE) {
+        # Stop if the user doesn't have the readxl package installed
+        if (!requireNamespace("readxl")) {
+            stop(call. = FALSE,
+                 "The readxl package is required to show progress. Please install it and try again.")
+        }
+    }
+
+    # Get package pids
+    package <- arcticdatautils::get_package(mn, resource_map_pid, file_names = TRUE)
+
+    # Initialize data_pids, return if no data present
+    if (length(package$data) == 0) {
+        return(invisible())
+    } else {
+        data_pids <- package$data
+    }
+
+    # Create file names
+    # file_names <- sapply(names(data_pids), function(i) {
+    #     ifelse(is.na(file_names[i]),
+    #            gsub('[^[:alnum:]]', '_', package$data[i]),
+    #            file_names[i])})
+
+    file_names <- names(data_pids)
+    prefix <- character(0)
+    if (prefix_file_names == TRUE) {
+        prefix <- remove_special_characters(package$metadata)
+        file_names <- paste0(prefix, "_", file_names)
+    }
+    out_paths <- file.path(download_directory, file_names)
+
+    download_data_objects(mn, data_pids, out_paths)
+
+    if (download_column_metadata == TRUE) {
+        eml <- EML::read_eml(rawToChar(dataone::getObject(mn, package$metadata)))
+        download_eml_attributes(eml, download_directory, prefix_file_names)
+    }
+
+    if (convert_excel_to_csv == TRUE) {
+
+    }
+
+    return(invisible())
+}
+
 #' Download a Data Package
 #'
 #' This function downloads all of the Data Objects in a Data Package to the local filesystem.
@@ -172,7 +283,7 @@ convert_bytes <- function(download_size) {
 #'
 #' @param mn (MNode) The Member Node to download from.
 #' @param resource_map_pid (chraracter) The identifier of the Resource Map for the package to download.
-#' @param download_directory (character) The path of the directory to download the package to. Defaults to the current working directory.
+#' @param download_directory (character) The path of the directory to download the package to.
 #' @param check_download_size (logical) Optional.  Whether to check the total download size before continuing.  Setting this to FALSE speeds up the function, especially when the package has many elements.
 #' @param download_child_packages (logical) Optional.  Whether to download data from child packages of the selected package.
 #' @param prefix_file_names (logical) Optional.  Whether to prefix file names with the package metadata identifier.  This is useful when downloading files from multiple packages to one directory.
@@ -191,178 +302,25 @@ convert_bytes <- function(download_size) {
 #' download_package(mn, "resource_map_urn:uuid:2b4e4174-4e4b-4a46-8ab0-cc032eda8269")
 #' }
 #'
-download_package <- function(mn,
-                             resource_map_pid,
-                             download_directory = getwd(),
-                             check_download_size = TRUE,
-                             download_child_packages = TRUE,
-                             prefix_file_names = FALSE,
-                             convert_excel_to_csv = FALSE,
-                             download_column_metadata = FALSE,
-                             check_first = TRUE) {
-    # TODO How many child levels should it support, currently one. 3 or 4
-    # max - could probably do this with a while loop.
-    # TODO resource_map_pid argument accepts metadata pids - could change to
-    # not accept metadata pids
-    # TODO Add option for downloading in folder structure that mirrors nesting
-    # rather than only all files in one folder
-    # TODO Convert check download size to helper function?
+child_packages <- list()
 
-    # Stop if the user doesn't have the readxl package installed
-    if (!requireNamespace("readxl")) {
-        stop(call. = FALSE,
-             "The readxl package is required to show progress. Please install it and try again.")
-    }
+# Get child package pids
+if (download_child_packages == TRUE) {
+    # Check that child packages exist
+    if (length(package$child_packages) != 0) {
+        n_child_packages <- length(package$child_packages)
+        progressBar <- utils::txtProgressBar(min = 0, max = n_child_packages, style = 3)
 
-    # Check that input arguments are in the correct format
-    stopifnot(is.character(resource_map_pid))
-    stopifnot(is.character(download_directory))
-    stopifnot(file.exists(download_directory))
-    stopifnot(is.logical(check_download_size))
-    stopifnot(is.logical(download_child_packages))
-    stopifnot(is.logical(check_first))
+        message("\nDownloading identifiers from child packages...")
 
-    # Get package pids
-    package <- arcticdatautils::get_package(mn, resource_map_pid, file_names = TRUE)
-    child_packages <- list()
-
-    # Get child package pids
-    if (download_child_packages == TRUE) {
-        # Check that child packages exist
-        if (length(package$child_packages) != 0) {
-            n_child_packages <- length(package$child_packages)
-            progressBar <- utils::txtProgressBar(min = 0, max = n_child_packages, style = 3)
-
-            message("\nDownloading identifiers from child packages...")
-
-            # Loop through child packages and extract pids using get_package()
-            child_packages <- lapply(seq_len(n_child_packages), function(i) {
-                utils::setTxtProgressBar(progressBar, i)
-                arcticdatautils::get_package(mn, package$child_packages[i], file_names = TRUE)
-            })
-
-            close(progressBar)
-        }
-    }
-
-    # Initialize data pids and filename prefixes vectors
-    data_pids <- vector("character")
-    filename_prefixes <- vector("character")
-
-    # Select data pids from initial package, if they exist
-    if (length(package$data) != 0) {
-        data_pids <- package$data
-    }
-
-    # Select data pids from child packages and add to data_pids
-    child_data_pids <- unlist(lapply(child_packages, function(package) {
-        return(package$data)
-    }))
-
-    data_pids <- c(data_pids, child_data_pids)
-
-    # Check that data exists
-    if (length(data_pids) == 0) {
-        message(warning("No data selected.  Double check the package(s) you entered contain data"))
-
-    } else {
-
-        # Check total download size
-        if (check_download_size) {
-            child_package_resource_map_pids <- lapply(child_packages, function(package) {
-                package$resource_map
-            })
-
-            download_size <- sum(
-                vapply(c(resource_map_pid, child_package_resource_map_pids), function(pid) {
-                    get_package_size(mn, pid)
-                }, 0)
-            )
-
-            download_size_message <- convert_bytes(download_size)
-
-            # Prompt user if they wish to continue based on total download size
-            message(paste0("\nYour download is approximately ", download_size_message, "\n"))
-            continue <- readline(prompt = paste0("Proceed with the download (", download_size_message, ")? Input yes/no: "))
-
-            while (!(continue %in% c("yes", "no"))) {
-                message("Type yes or no without quotation marks or capitals\n")
-                continue <- readline(prompt = "Proceed with the download? Input yes/no: ")
-            }
-
-            # Cancel download if "no" was entered
-            if (continue == "no") {
-                stop("Download cancelled by user")
-            }
-        }
-
-        # Create filename prefixes
-        if (prefix_file_names == TRUE) {
-            # List all packages together
-            if (length(child_packages) > 0) {
-                all_packages <- append_lists(list(package), child_packages)
-            } else {
-                all_packages <- list(package)
-            }
-
-            # Remove special characters from each package metadata identifier and rep for # of data objects in each package
-            filename_prefixes <- unlist(lapply(all_packages, function(all_packages) {
-                prefix <- remove_special_characters(all_packages$metadata)
-                return(rep(prefix, length(all_packages$data)))
-            }))
-        }
-
-        # Download data pids to selected directory
-        n_data_objects <- length(data_pids)
-        file_names <- names(data_pids)
-        progressBar <- utils::txtProgressBar(min = 0, max = n_data_objects, style = 3)
-        message(paste0("\nDownloading data objects to ", download_directory))
-
-        lapply(seq_len(n_data_objects), function(i) {
-            # If file_names[i] is NA, name it using the pid
-            file_name <- ifelse(is.na(file_names[i]), gsub('[^[:alnum:]]', '_', data_pids[i]), file_names[i])
-
-            if (prefix_file_names == TRUE) {
-                file_name <- paste0(filename_prefixes[i], "__", file_names[i])
-            }
-            out_path <- file.path(download_directory, file_name)
-
-            if (file.exists(out_path)) {
-                warning(call. = FALSE,
-                        paste0("The file ", out_path, " already exists. Skipping download."))
-            } else {
-                # Attempt to download object up to 3 times
-                n_tries <- 0
-                dataObj <- "download_error"
-
-                while (dataObj[1] == "download_error" & n_tries < 3) {
-                    dataObj <- tryCatch(dataone::getObject(mn,
-                                                           data_pids[i],
-                                                           check = check_first),
-                                        error = function(e) {return("download_error")})
-                    n_tries <- n_tries + 1
-                }
-
-                tryCatch(writeBin(dataObj, out_path), error = function(e) {
-                    message(paste0("\n Unable to download ", file_name, ". Please download manually or
-                               contact the Arctic Data Center for assistance."))
-                })
-
-                # Convert excel workbooks to csv
-                if (convert_excel_to_csv == TRUE) {
-                    if (grepl(".xls", out_path, ignore.case = TRUE)) {
-                        excel_to_csv_prefix(out_path, filename_prefixes[i])
-                    }
-                }
-            }
-
+        # Loop through child packages and extract pids using get_package()
+        child_packages <- lapply(seq_len(n_child_packages), function(i) {
             utils::setTxtProgressBar(progressBar, i)
+            arcticdatautils::get_package(mn, package$child_packages[i], file_names = TRUE)
         })
 
         close(progressBar)
     }
-
-    return(invisible())
 }
 
 #' Download one or multiple Data Packages
@@ -405,3 +363,33 @@ download_packages <- function(mn, resource_map_pids, ...) {
 
     return(invisible())
 }
+
+
+# # Check total download size
+# if (check_download_size) {
+#     child_package_resource_map_pids <- lapply(child_packages, function(package) {
+#         package$resource_map
+#     })
+#
+#     download_size <- sum(
+#         vapply(c(resource_map_pid, child_package_resource_map_pids), function(pid) {
+#             get_package_size(mn, pid)
+#         }, 0)
+#     )
+#
+#     download_size_message <- convert_bytes(download_size)
+#
+#     # Prompt user if they wish to continue based on total download size
+#     message(paste0("\nYour download is approximately ", download_size_message, "\n"))
+#     continue <- readline(prompt = paste0("Proceed with the download (", download_size_message, ")? Input yes/no: "))
+#
+#     while (!(continue %in% c("yes", "no"))) {
+#         message("Type yes or no without quotation marks or capitals\n")
+#         continue <- readline(prompt = "Proceed with the download? Input yes/no: ")
+#     }
+#
+#     # Cancel download if "no" was entered
+#     if (continue == "no") {
+#         stop("Download cancelled by user")
+#     }
+# }
