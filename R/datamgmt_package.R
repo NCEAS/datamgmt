01eb3433154636a44f2f5341e3aa459dfd8b8ec0
Irene <25118334+isteves@users.noreply.github.com>
2018-04-13 14:14:02 -0700

    added skeleton package documentation

---
+++ R/datamgmt_package.R
@@ -0,0 +1,13 @@
+#' Data management utilities for curating, documenting, and publishing data
+#'
+#' The datamgmt R package supports management of data
+#' packages on the Arctic Data Center (ADC) and State
+#' of Alaska's Salmon and People (SASAP) data portals.
+#'
+#' @docType package
+#' @rdname datamgmt
+#' @name datamgmt
+#' @author Data Science Fellows, Spring 2018: Dominic Mullen, Irene Steves, Mitchell Maier, Stephanie Freund, Emily O'Dean (NCEAS)
+#'
+#' @seealso \href{https://nceas.github.io/datamgmt/}{datamgmt pkgdown website}
+NULL
