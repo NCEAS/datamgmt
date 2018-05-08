66bc4b6757fe632c2dda33764d0eb2dfd3c88119
Irene <25118334+isteves@users.noreply.github.com>
2018-05-08 16:10:09 -0700

    removed datamgmt:: and added comment; might want to remove one of these tests - excel_to_csv should REQUIRE excel files as inputs

--- tests/testthat/test_excel_to_csv.R
+++ tests/testthat/test_excel_to_csv.R
@@ -6,18 +6,19 @@ test_that("csv files are written correctly", {
                         "example_excel.xlsx"),
               excel_path)
 
-    datamgmt::excel_to_csv(excel_path, tempdir())
+    excel_to_csv(excel_path, tempdir())
     file_paths <- file.path(tempdir(), c("dummy_Sheet1.csv", "dummy_Sheet2.csv"))
 
     expect_equal(all(file.exists(file_paths)), TRUE)
 })
 
 test_that("excel_to_csv doesn't stop if file is not excel format", {
+    #might want to reconsider this test ^
     dummy_data <- data.frame(seq(1:5))
 
     path <- file.path(tempdir(), "dummy.Rdata")
     save(dummy_data, file = path)
 
     # Will output a message but will not error
-    expect_message(datamgmt::excel_to_csv(path))
+    expect_message(excel_to_csv(path))
 })
