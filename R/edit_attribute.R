7a9d51e5054897c7a6af1e131e46c1bfe9776781
Derek Strong <dstrong@nceas.ucsb.edu>
2018-10-04 12:46:28 -0700

    Change edit_attribute to support any type of data object

--- R/edit_attribute.R
+++ R/edit_attribute.R
@@ -1,61 +1,84 @@
 #' Edit a single attribute
 #'
-#' This function edits the slots of a single attribute in an existing attribute table.
+#' This function edits the slots of a single attribute in an existing list of attributes for a data object.
 #'
-#' Can only be used on attributes entirely defined within the "attributes" slot of attributeList; it cannot be used to edit
-#' the factor table of an enumeratedDomain. In cases with very large attribute lists, user may want to use
-#' [which_in_eml()] first to locate the attribute position.
+#' This function can only be used on attributes entirely defined within the 'attributes' slot of attributeList;
+#' it cannot be used to edit the factors table of an enumeratedDomain.
 #'
-#' @param eml The eml object containing the attributeList
-#' @param dataTableNumber The number of the dataTable containing the attributeList; in other words its position in the dataTable list.
-#' @param attributeNumber The number of the attribute of interest; in other words its position in the attributeList.
-#' @param attributeName The new name to give to the attribute
-#' @param attributeDefinition The new attributeDefinition to give to the attribute
-#' @param domain The new domain to give to the attribute
-#' @param measurementScale The new measurementScale to give to the attribute
-#' @param unit The new unit to give to the attribute
-#' @param numberType The new numberType to give to the attribute
-#' @param definition The new definition (for textDomain) to give to the attribute
-#' @param formatString The new formatString to give to the attribute
-#' @param missingValueCode The new missing value code to give to the attribute
-#' @param missingValueCodeExplanation The new missing value code explanation to give to the attribute
+#' In cases with very large attribute lists, use [which_in_eml()] first to locate
+#' the attribute index number in the attributeList.
+#'
+#' @param attribute (attribute) The attribute in the the attributeList of a data object.
+#' @param attributeName (character) The new name to give to the attribute.
+#' @param attributeLabel (character) The new label to give to the attribute.
+#' @param attributeDefinition (character) The new attributeDefinition to give to the attribute.
+#' @param domain (character) The new domain to give to the attribute.
+#' @param measurementScale (character) The new measurementScale to give to the attribute.
+#' @param unit (character) The new unit (for numericDomain) to give to the attribute.
+#' @param numberType (character) The new numberType (for numericDomain) to give to the attribute.
+#' @param definition (character) The new definition (for textDomain) to give to the attribute.
+#' @param formatString (character) The new formatString (for dateTime) to give to the attribute.
+#' @param missingValueCode (character) The new missing value code to give to the attribute.
+#' @param missingValueCodeExplanation (character) The new missing value code explanation to give to the attribute.
+#'
+#' @return (attribute) The modified attribute.
+#'
+#' @import EML
+#' @importFrom methods new
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' # To change an attribute's name and add a missing value code:
-#' new_eml <- edit_attribute(eml, 2, 8, attributeName = "new name", missingValueCode = "NA",
-#'                           missingValueCodeExplanation = "Data unavailable")
+#' # Change an attribute's name
+#' new_attribute <- edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[8]],
+#' attributeName = "new name")
+#' eml@dataset@dataTable[[1]]@attributeList@attribute[[8]] <- new_attribute
 #'
-#' # To change an attribute's attributeDefinition, as well as change measurementScale
-#' # from nominal to ratio (requires also changing domain, unit, and numberType, as well
-#' # as setting definition to NA):
-#' new_eml <- edit_attribute(eml, 1, 2, attributeDefinition = "new definition",
+#' # Change an attribute's measurementScale from nominal to ratio
+#' # (also requires changing domain, unit, and numberType as well
+#' # as setting definition to NA)
+#' new_attribute <- edit_attribute(eml@dataset@otherEntity[[2]]@attributeList@attribute[[1]],
 #' domain = "numericDomain", measurementScale = "ratio", unit = "dimensionless",
 #' numberType = "whole", definition = NA)
+#' eml@dataset@otherEntity[[2]]@attributeList@attribute[[1]] <- new_attribute
+#'
+#' # Add the same missing value codes to all attributes for a data object
+#' new_attributes <- lapply(eml@dataset@dataTable[[1]]@attributeList@attribute, edit_attribute,
+#' missingValueCode = "NA", missingValueCodeExplanation = "data unavailable")
+#' eml@dataset@dataTable[[1]]@attributeList@attribute <- new_attributes
 #' }
-edit_attribute <- function(eml, dataTableNumber, attributeNumber, attributeName = NULL, attributeDefinition = NULL, domain=NULL,
+edit_attribute <- function(attribute, attributeName = NULL, attributeLabel = NULL, attributeDefinition = NULL, domain = NULL,
                            measurementScale = NULL, unit = NULL, numberType = NULL, definition = NULL, formatString = NULL,
-                           missingValueCode = NULL, missingValueCodeExplanation = NULL){
+                           missingValueCode = NULL, missingValueCodeExplanation = NULL) {
+    stopifnot(methods::is(attribute, "attribute"))
+    if (!is.null(attributeName)) stopifnot(is.character(attributeName) && nchar(attributeName) > 0)
+    if (!is.null(attributeLabel) && !is.na(attributeLabel)) stopifnot(is.character(attributeLabel) && nchar(attributeLabel) > 0)
+    if (!is.null(attributeDefinition)) stopifnot(is.character(attributeDefinition) && nchar(attributeDefinition) > 0)
+    if (!is.null(domain)) stopifnot(is.character(domain) && any(c("textDomain", "enumeratedDomain", "numericDomain", "dateTime") %in% domain))
+    if (!is.null(measurementScale)) stopifnot(is.character(measurementScale) && any(c("nominal", "ordinal", "dateTime", "ratio", "interval") %in% measurementScale))
+    if (!is.null(unit) && !is.na(unit)) stopifnot(is.character(unit) && nchar(unit) > 0)
+    if (!is.null(numberType) && !is.na(numberType)) stopifnot(is.character(numberType) && any(c("real", "natural", "whole", "integer") %in% numberType))
+    if (!is.null(definition) && !is.na(definition)) stopifnot(is.character(definition) && nchar(definition) > 0)
+    if (!is.null(formatString) && !is.na(formatString)) stopifnot(is.character(formatString) && nchar(formatString) > 0)
+    if (!is.null(missingValueCode) && !is.na(missingValueCode)) stopifnot(is.character(missingValueCode) && nchar(missingValueCode) > 0)
+    if (!is.null(missingValueCodeExplanation) && !is.na(missingValueCodeExplanation)) stopifnot(is.character(missingValueCodeExplanation) && nchar(missingValueCodeExplanation) > 0)
+    if (length(c(missingValueCode, missingValueCodeExplanation)) == 1) stop("Need both missingValueCode and missingValueCodeExplanation.")
 
-    data <- EML::get_attributes(eml@dataset@dataTable[[dataTableNumber]]@attributeList)
-    attributeTable <-data$attributes #this excludes the factor table from enumerated domain.
+    # Assign attribute to attributeList in order to convert attribute slots to data.frame
+    attList <- methods::new("attributeList")
+    attList@attribute[[1]] <- attribute
+    data <- EML::get_attributes(attList)$attributes
 
-    if(length(c(missingValueCode, missingValueCodeExplanation)) == 1){
-        stop("Need both missingValueCode and missingValueCodeExplanation")
+    attribute_edits <- cbind(attributeName, attributeLabel, attributeDefinition, domain,
+                             measurementScale, unit, numberType, definition, formatString,
+                             missingValueCode, missingValueCodeExplanation)
+
+    for (i in colnames(attribute_edits)) {
+        data[ , i] <- attribute_edits[ , i]
     }
 
-    attribute_edits <- cbind(attributeName, attributeDefinition, domain,
-                        measurementScale, unit, numberType, definition, formatString,
-                        missingValueCode, missingValueCodeExplanation)
-
-    for(i in colnames(attribute_edits)){
-        attributeTable[,i][attributeNumber] <- attribute_edits[,i]
-    }
-
-    attribute_list <- EML::set_attributes(attributeTable, factors = data$factors)
-    eml@dataset@dataTable[[dataTableNumber]]@attributeList <- attribute_list
-    EML:::check_and_complete_attributes(attributeTable, NULL)
-    return(eml)
+    # Set edits to attributeList in order to convert data.frame to new attribute
+    new_attribute <- EML::set_attributes(data)@attribute[[1]]
+    new_attribute
 }
--- tests/testthat/test_edit_attribute.R
+++ tests/testthat/test_edit_attribute.R
@@ -2,87 +2,101 @@ context("edit_attribute")
 
 eml_path <- system.file("dummy_eml_w_attributes.xml", package = "datamgmt")
 eml <- EML::read_eml(eml_path)
+
 eml_key_path <- system.file("key_eml_w_attributes.xml", package = "datamgmt")
 eml_key <- EML::read_eml(eml_key_path)
 
+
+test_that("inputs are correct", {
+    expect_error(edit_attribute(7))
+    expect_error(edit_attribute("test"))
+    expect_error(edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute))
+    expect_error(edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[1]],
+                                attributeName = 7))
+    expect_error(edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[1]],
+                                domain = ""))
+    expect_error(edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[1]],
+                                domain = "text"))
+    expect_error(edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[1]],
+                                measurementScale = "other"))
+    expect_error(edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[1]],
+                                numberType = "other"))
+    expect_error(edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[1]],
+                                missingValueCode = "NA"))
+    expect_error(edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[1]],
+                                missingValueCodeExplanation = "data unavailable"))
+})
+
+
 test_that("attributes are updated in correct slots", {
+    new_attribute <- edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[2]],
+                                    attributeDefinition = "test definition",
+                                    domain = "numericDomain",
+                                    measurementScale = "ratio",
+                                    unit = "dimensionless",
+                                    numberType = "whole",
+                                    definition = NA)
 
-    eml_test <- edit_attribute(eml, 1, 2,
-                               attributeDefinition = "test definition",
-                               domain = "numericDomain",
-                               measurementScale = "ratio",
-                               unit = "dimensionless",
-                               numberType = "whole",
-                               definition = NA)
-
-    eml_attr_test <- eml_test@dataset@dataTable[[1]]@attributeList@attribute[[2]]
     eml_attr_key <- eml_key@dataset@dataTable[[1]]@attributeList@attribute[[2]]
 
-    expect_match(eml_attr_test@attributeDefinition,
+    expect_match(new_attribute@attributeDefinition,
                  "test definition")
 
-    expect_equivalent(eml_attr_test@measurementScale,
+    expect_equivalent(new_attribute@measurementScale,
                       eml_attr_key@measurementScale)
 
-    expect_equivalent(eml_attr_test@measurementScale@ratio,
+    expect_equivalent(new_attribute@measurementScale@ratio,
                       eml_attr_key@measurementScale@ratio)
 
-    expect_equivalent(eml_attr_test@measurementScale@ratio@unit,
+    expect_equivalent(new_attribute@measurementScale@ratio@unit,
                       eml_attr_key@measurementScale@ratio@unit)
 
-    expect_match(eml_attr_test@measurementScale@ratio@numericDomain@numberType,
+    expect_match(new_attribute@measurementScale@ratio@numericDomain@numberType,
                  "whole")
 })
 
-test_that("existing attributes that aren't changed in update are retained", {
 
-    eml_test <- edit_attribute(eml, 1, 2,
-                               attributeDefinition = "test definition",
-                               domain = "numericDomain",
-                               measurementScale = "ratio",
-                               unit = "dimensionless",
-                               numberType = "whole",
-                               definition = NA)
+test_that("existing attributes that are not changed in update are retained", {
+    new_attribute <- edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[2]],
+                                    attributeDefinition = "test definition",
+                                    domain = "numericDomain",
+                                    measurementScale = "ratio",
+                                    unit = "dimensionless",
+                                    numberType = "whole",
+                                    definition = NA)
 
-    expect_match(eml_test@dataset@dataTable[[1]]@attributeList@attribute[[2]]@attributeName,
+    expect_match(new_attribute@attributeName,
                  "col2")
 })
 
-test_that("slots that no longer apply to updated domain type are gone", {
-    eml_test <- edit_attribute(eml, 1, 2,
-                               attributeDefinition = "test definition",
-                               domain = "numericDomain",
-                               measurementScale = "ratio",
-                               unit = "dimensionless",
-                               numberType = "whole",
-                               definition = NA)
 
-    expect_error(eml_test@dataset@dataTable[[1]]@attributeList@attribute[[2]]@measurementScale@nominal@nonNumericDomain@textDomain[[1]]@definition)
+test_that("slots that no longer apply to updated domain type are gone", {
+    new_attribute <- edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[2]],
+                                    attributeDefinition = "test definition",
+                                    domain = "numericDomain",
+                                    measurementScale = "ratio",
+                                    unit = "dimensionless",
+                                    numberType = "whole",
+                                    definition = NA)
+
+    expect_error(new_attribute@measurementScale@nominal@nonNumericDomain@textDomain[[1]]@definition)
 })
 
-test_that("error occurs if incorrect combinations occur between measurementScale and domain", {
 
-    expect_error(edit_attribute(eml, 1, 2,
+test_that("error occurs if incorrect combinations occur between measurementScale and domain", {
+    expect_error(edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[2]],
                                 attributeDefinition = "test definition",
                                 domain = "textDomain",
                                 measurementScale = "ratio",
                                 unit = "dimensionless",
                                 numberType = "whole",
-                                definition = 'NA'))
+                                definition = NA))
 
-    expect_error(edit_attribute(eml, 1, 2,
+    expect_error(edit_attribute(eml@dataset@dataTable[[1]]@attributeList@attribute[[2]],
                                 attributeDefinition = "test definition",
                                 domain = "numericDomain",
                                 measurementScale = "dateTime",
                                 unit = "dimensionless",
                                 numberType = "whole",
-                                definition = 'NA'))
-})
-
-test_that("error occurs if you specify either missingValueCode or missing ValueCodeExplanation but not both", {
-
-    expect_error(edit_attribute(eml, 1, 2,
-                                missingValueCode = "testValueCode"))
-    expect_error(edit_attribute(eml, 1, 2,
-                                missingValueCode = "testExplanation"))
+                                definition = NA))
 })
