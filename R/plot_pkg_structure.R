7b7a882422c9f885c50813b78a6a36d127bfcfa4
Derek Strong <dstrong@nceas.ucsb.edu>
2018-10-22 14:57:01 -0700

    Refactor plot_pkg_structure()

--- R/plot_pkg_structure.R
+++ R/plot_pkg_structure.R
@@ -1,42 +1,48 @@
 #' Query tree
 #'
 #' This is a helper function for [plot_pkg_structure()]
-#' that allows you to start with a (grand)parent PID and recursively
-#' run Solr queries.
+#' that starts with a (grand)parent PID and recursively
+#' runs Solr queries to find all resource map PIDs in a
+#' data package family.
 #'
-#' @param mn The Member Node.
-#' @param parent_rm_pid (character) The top-level PID to use for the query.
+#' @param mn (MNode)The Member Node to query.
+#' @param parent_rm_pid (character) The top-level PID in a data package family.
 #'
-#' @import magrittr
-#' @import stringr
-#' @importFrom tidyr unnest
+#' @return (data.frame) A data frame of results.
+#'
+#' @import dataone
+#' @importFrom dplyr bind_rows
 #'
 #' @noRd
 #'
 #' @examples
 #' \dontrun{
-#' cn <- dataone::CNode("PROD")
-#' mn <- dataone::getMNode(cn,"urn:node:ARCTIC")
+#' cn <- CNode("PROD")
+#' mn <- getMNode(cn,"urn:node:ARCTIC")
 #'
-#' parent_rm_pid <- "urn:uuid:b3dc11f5-95e8-4f30-bef9-82464398bc5f"
+#' parent_rm_pid <- "urn:uuid:..."
 #'
 #' query_tree(mn, parent_rm_pid)
 #' }
-query_tree <- function(mn, parent_rm_pid){
+query_tree <- function(mn, parent_rm_pid) {
+    stopifnot(class(mn) %in% c("MNode", "CNode"))
+    stopifnot(is.character(parent_rm_pid), nchar(parent_rm_pid) > 0)
+
     # Initialize structure
-    if(!exists("rm_all")){
+    if (!exists("rm_all")) {
         rm_all <- NULL
     }
 
-    # Run query & clean results
-    result <- dataone::query(mn, list(q = paste0('resourceMap:"', parent_rm_pid, '"+AND+formatType:RESOURCE'),
-                                      fl = 'identifier, resourceMap',
-                                      sort = 'dateUploaded+desc',
-                                      rows='10000'),
+    # Run query and clean results
+    result <- dataone::query(mn,
+                             list(q = paste0('resourceMap:"', parent_rm_pid, '"+AND+formatType:RESOURCE'),
+                                  fl = 'identifier, resourceMap',
+                                  sort = 'dateUploaded+desc',
+                                  rows = '10000'),
                              as = "data.frame")
 
     # Run recursion
-    if(nrow(result) > 0){
+    if (nrow(result) > 0) {
         rm_all <- dplyr::bind_rows(rm_all, result)
 
         for (rm_pid in result$identifier) {
@@ -44,66 +50,62 @@ query_tree <- function(mn, parent_rm_pid){
             rm_all <- dplyr::bind_rows(rm_all, result2)
         }
     }
+
     return(rm_all)
 }
 
 
 #' Plot package structure
 #'
-#' This function allows you to quickly visualize how data packages
+#' This function visualizes how data packages in a data package family
 #' are related to each other.
 #'
-#' @param mn The Member Node.
+#' @param mn (MNode) The Member Node to query.
 #' @param parent_rm_pid (character) The top-level PID in a data package family.
 #'
-#' @import dplyr
+#' @return A visIgraph plot.
+#'
+#' @import magrittr
+#' @importFrom dplyr mutate rename mutate_all funs distinct select
+#' @importFrom stringr str_split str_replace
 #' @importFrom tidyr unnest
 #'
 #' @export
 #'
 #' @examples
 #' \dontrun{
-#' cn <- dataone::CNode('PROD')
-#' mn <- dataone::getMNode(cn,'urn:node:ARCTIC')
+#' cn <- CNode("PROD")
+#' mn <- getMNode(cn,"urn:node:ARCTIC")
 #'
-#' parent_rm_pid <- "resource_map_urn:uuid:2b5f44fc-810e-4c30-a63f-5bd2f3ff00a7"
+#' parent_rm_pid <- "resource_map_urn:uuid:..."
 #'
 #' plot_pkg_structure(mn, parent_rm_pid)
 #' }
 plot_pkg_structure <- function(mn, parent_rm_pid) {
+    stopifnot(class(mn) %in% c("MNode", "CNode"))
+    stopifnot(is.character(parent_rm_pid), nchar(parent_rm_pid) > 0)
 
-    if (!requireNamespace("visNetwork", quietly = TRUE) | !requireNamespace("igraph", quietly = TRUE) | !requireNamespace("purrr", quietly = TRUE)) {
+    # Include required packages in Suggests instead of Imports in DESCRIPTION file
+    if (!requireNamespace("visNetwork", quietly = TRUE) || !requireNamespace("igraph", quietly = TRUE)) {
         stop(call. = FALSE,
-             "plot_pkg_structure requires the
-         'visNetwork', 'igraph', and 'purrr' packages to be installed. Install these packages for this functionality.")
-
-        #removed importFroms:
-
-        # @importFrom visNetwork visIgraph
-        # @importFrom igraph graph_from_data_frame
-        # @importFrom purrr map
-    }
-
-    # Check that the pid is a resource map
-    if(!str_detect(parent_rm_pid, "resource")){
-        warning(cat("Is", parent_rm_pid, "a resource map PID? Please double check.\n"))
+             paste("plot_pkg_structure() requires the visNetwork and igraph packages to be installed.",
+                   "Install these packages to use this function."))
     }
 
     query_results <- query_tree(mn, parent_rm_pid)
-    if(!nrow(query_results) > 0){
-        stop("The query did not return any results. Check the member node and parent resource map.")
+
+    if (is.null(query_results)) {
+        stop("The query did not return any results. Check the Member Node and parent resource map PID.")
     }
 
     edges <- query_results %>%
-        # as_tibble() %>%
-        dplyr::mutate(resourceMap = str_split(resourceMap, " ")) %>%
-        dplyr::mutate(resourceMap = purrr::map(resourceMap, function(vector){vector[length(vector)]})) %>% #get last element
-        tidyr::unnest(resourceMap) %>%
-        dplyr::rename(from = resourceMap,
-                      to = identifier) %>%
-        dplyr::mutate_all(funs(stringr::str_replace(., "resource_map_", ""))) %>%
+        dplyr::mutate("resourceMap" = stringr::str_split(.$resourceMap, " ")) %>% # separate all resource map versions
+        dplyr::mutate("resourceMap" = lapply(.$resourceMap, function(vector) vector[length(vector)])) %>% # get latest resource map
+        tidyr::unnest() %>% # unnest resourceMap list column
+        dplyr::rename("from" = "resourceMap", "to" = "identifier") %>%
+        dplyr::mutate_all(dplyr::funs(stringr::str_replace(., "resource_map_", ""))) %>%
         dplyr::distinct() %>%
-        dplyr::select(from, to)
+        dplyr::select("from", "to")
 
     rm_igraph <- igraph::graph_from_data_frame(d = edges, directed = TRUE)
 
@@ -111,3 +113,7 @@ plot_pkg_structure <- function(mn, parent_rm_pid) {
                           layout = "layout_as_tree",
                           flip.y = FALSE)
 }
+
+
+# Quiets concerns of R CMD check about no visible binding for global variable ‘.’
+if (getRversion() >= "2.15.1") utils::globalVariables(c("."))
---
+++ tests/testthat/test_plot_pkg_structure.R
@@ -0,0 +1,15 @@
+context("Plot package structure")
+
+cn <- dataone::CNode("PROD")
+mn <- dataone::getMNode(cn,"urn:node:ARCTIC")
+
+
+test_that("plot_pkg_structure() accepts correct inputs", {
+    expect_error(plot_pkg_structure(7, "test"))
+    expect_error(plot_pkg_structure("test", "test"))
+    expect_error(plot_pkg_structure(list("test"), "test"))
+
+    expect_error(plot_pkg_structure(mn, ""))
+    expect_error(plot_pkg_structure(mn, 7))
+    expect_error(plot_pkg_structure(mn, list("test")))
+})
