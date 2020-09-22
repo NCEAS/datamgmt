53ef412862e5d3d4ac620ccca4476075b56f5b33
laijasmine <jasminelai@nceas.ucsb.edu>
2020-09-22 09:58:18 -0700

    skip the tests in Travis

--- tests/testthat/test_plot_pkg_structure.R
+++ tests/testthat/test_plot_pkg_structure.R
@@ -1,5 +1,7 @@
 context("Plot package structure")
 
+testthat::skip_on_travis()
+
 cn <- dataone::CNode("PROD")
 mn <- dataone::getMNode(cn,"urn:node:ARCTIC")
 
--- tests/testthat/test_qa_package.R
+++ tests/testthat/test_qa_package.R
@@ -1,5 +1,7 @@
 context("QA package")
 
+testthat::skip_on_travis()
+
 cn_prod <- CNode("PROD")
 adc_prod <- getMNode(cn_prod, "urn:node:ARCTIC")
 
